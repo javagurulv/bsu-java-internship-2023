0f796ad6a375247c8c0e481994da3bfe4c146207
katierevinska <katerevinska05@gmail.com>
2023-11-12 12:19:57 +0300

    step_4 Done

--- student_revinskaya_ekaterina/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_revinskaya_ekaterina/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -4,8 +4,14 @@ import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 
+import java.math.BigDecimal;
+import java.util.Date;
+
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
+    private BigDecimal calculateDiffBetweenDays(Date date1, Date date2){
+        return BigDecimal.valueOf(date1.getTime() - date2.getTime());
+    }
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
@@ -14,6 +20,8 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         response.setPersonLastName(request.getPersonLastName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setAgreementDateTo(request.getAgreementDateTo());
+
+        response.setAgreementPrice(calculateDiffBetweenDays(response.getAgreementDateFrom(), response.getAgreementDateTo()));
         return response;
     }
 
