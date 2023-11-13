458fcb435c32ff8febcad980cf7285523951db0e
blackberry020 <ilonafrolova00@gmail.com>
2023-11-13 19:29:38 +0300

    step_6 upd

--- student_frolova_ilona/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_frolova_ilona/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -5,6 +5,7 @@ import lv.javaguru.travel.insurance.dto.CoreResponse;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Getter @Setter
 @NoArgsConstructor
@@ -17,4 +18,8 @@ public class TravelCalculatePremiumResponse extends CoreResponse {
     private Date agreementDateTo;
 
     private BigDecimal agreementPrice;
+
+    public TravelCalculatePremiumResponse(List<ValidationError> errors) {
+        super(errors);
+    }
 }
