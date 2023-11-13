1a47d1fd824d161bb8e9966523ce294e00d7e43b
Matvei Lobosov <lobosov.matvei@gmail.com>
2023-11-13 13:55:00 +0300

    step_4 Done

--- student_lobosov_matvei/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_lobosov_matvei/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -5,6 +5,8 @@ import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.springframework.stereotype.Component;
 import java.math.BigDecimal;
 
+import static lv.javaguru.travel.insurance.core.DateTimeService.getDaysBetween;
+
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
@@ -15,6 +17,7 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         response.setPersonLastName(request.getPersonLastName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setAgreementDateTo(request.getAgreementDateTo());
+        response.setAgreementPrice(getDaysBetween(request.getAgreementDateTo(), request.getAgreementDateFrom()));
 
         return response;
     }
