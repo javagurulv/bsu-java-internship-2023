3cc46efcc71742e0df874f5eab5051251741fb7a
Kreolancer <kreolancer@gmail.com>
2023-11-13 03:17:52 +0300

    step_5 = done

--- student_polyakov_andrey/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_polyakov_andrey/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -2,7 +2,15 @@ package lv.javaguru.travel.insurance.rest;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelCalculatePremiumResponse {
 
     private String personFirstName;
@@ -10,47 +18,5 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
     private BigDecimal agreementPrice;
-
-    public TravelCalculatePremiumResponse() {}
-
-    public String getPersonFirstName() {
-        return personFirstName;
-    }
-
-    public void setPersonFirstName(String personFirstName) {
-        this.personFirstName = personFirstName;
-    }
-
-    public String getPersonLastName() {
-        return personLastName;
-    }
-
-    public void setPersonLastName(String personLastName) {
-        this.personLastName = personLastName;
-    }
-
-    public Date getAgreementDateFrom() {
-        return agreementDateFrom;
-    }
-
-    public void setAgreementDateFrom(Date agreementDateFrom) {
-        this.agreementDateFrom = agreementDateFrom;
-    }
-
-    public Date getAgreementDateTo() {
-        return agreementDateTo;
-    }
-
-    public void setAgreementDateTo(Date agreementDateTo) {
-        this.agreementDateTo = agreementDateTo;
-    }
-
-    public BigDecimal getAgreementPrice() {
-        return agreementPrice;
-    }
-
-    public void setAgreementPrice(BigDecimal agreementPrice) {
-        this.agreementPrice = agreementPrice;
-    }
 
 }
