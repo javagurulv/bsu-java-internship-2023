7fca65e6b81129934ade99c2208718540b1e71af
Slate2d <slatedazty@yandex.ru>
2023-11-14 19:53:04 +0300

    step_5=DONE

--- student_golovach_vladislav/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_golovach_vladislav/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -1,12 +1,20 @@
 package lv.javaguru.travel.insurance.rest;
 
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
 import java.math.BigDecimal;
 import java.time.Duration;
 import java.time.LocalDate;
 import java.time.ZoneId;
 import java.util.Date;
 import java.util.concurrent.TimeUnit;
-
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
 public class TravelCalculatePremiumResponse {
     BigDecimal agreementPrice;
     private String personFirstName;
@@ -14,43 +22,6 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateFrom;
     private Date agreementDateTo;
 
-    public TravelCalculatePremiumResponse() {}
-
-    public String getPersonFirstName() {
-        return personFirstName;
-    }
-
-    public void setPersonFirstName(String personFirstName) {
-        this.personFirstName = personFirstName;
-    }
-
-    public String getPersonLastName() {
-        return personLastName;
-    }
-
-    public void setPersonLastName(String personLastName) {
-        this.personLastName = personLastName;
-    }
-
-    public Date getAgreementDateFrom() {
-        return agreementDateFrom;
-    }
-
-    public void setAgreementDateFrom(Date agreementDateFrom) {
-        this.agreementDateFrom = agreementDateFrom;
-    }
-
-    public Date getAgreementDateTo() {
-        return agreementDateTo;
-    }
-
-    public void setAgreementDateTo(Date agreementDateTo) {
-        this.agreementDateTo = agreementDateTo;
-    }
-
-    public void setAgreementPrice(BigDecimal agreementPrice) {
-        this.agreementPrice = agreementPrice;
-    }
     public long calculateDaysBetween(Date date1, Date date2) {
         long elapsedms = date1.getTime() - date2.getTime();
         return TimeUnit.DAYS.convert(elapsedms, TimeUnit.MILLISECONDS);
