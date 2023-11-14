f170e5bcfed1a4b41610bc9217797518110956dd
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2023-11-14 20:27:35 +0200

    step_8-11=DONE

--- student_snezhko_leu/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
+++ student_snezhko_leu/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
@@ -1,20 +1,23 @@
 package lv.javaguru.travel.insurance.core;
 
-import lv.javaguru.travel.insurance.core.TravelCalculatePremiumRequestValidator;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.Mockito.mock;
-import static org.mockito.Mockito.when;
 
-import java.util.ArrayList;
+import java.sql.Date;
 import java.util.List;
 
 public class TravelCalculatePremiumRequestValidatorTest {
     private List<ValidationError> errors;
-    private void before() {
+    private void beforeForNotNullTests() {
         TravelCalculatePremiumRequestValidator validator = new TravelCalculatePremiumRequestValidator();
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("", "", null, null);
+        errors = validator.validate(request);
+    }
+    private void beforeForDateSequenceTests() {
+        TravelCalculatePremiumRequestValidator validator = new TravelCalculatePremiumRequestValidator();
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("", "", new Date(25000), new Date(24000));
         errors = validator.validate(request);
     }
     private boolean isEqual(ValidationError e1, ValidationError e2) {
@@ -22,26 +25,32 @@ public class TravelCalculatePremiumRequestValidatorTest {
     }
     @Test
     public void TravelCalculatePremiumRequestValidatorFirstNameTest() {
-        this.before();
+        this.beforeForNotNullTests();
         ValidationError error = errors.get(0);
         assertTrue(isEqual(errors.get(0), new ValidationError("personFirstName", "Must not be empty!")));
     }
     @Test
     public void TravelCalculatePremiumRequestValidatorLastNameTest() {
-        this.before();
+        this.beforeForNotNullTests();
         ValidationError error = errors.get(1);
         assertTrue(isEqual(errors.get(1), new ValidationError("personLastName", "Must not be empty!")));
     }
     @Test
     public void TravelCalculatePremiumRequestValidatorAgreementDateFromTest() {
-        this.before();
+        this.beforeForNotNullTests();
         ValidationError error = errors.get(2);
         assertTrue(isEqual(errors.get(2), new ValidationError("agreementDateFrom", "Must not be null!")));
     }
     @Test
     public void TravelCalculatePremiumRequestValidatorAgreementDateToTest() {
-        this.before();
+        this.beforeForNotNullTests();
         ValidationError error = errors.get(3);
         assertTrue(isEqual(errors.get(3), new ValidationError("agreementDateTo", "Must not be null!")));
+    }
+    @Test
+    public void TravelCalculatePremiumRequestValidatorDateToIsNotBeforeDateFromTest() {
+        this.beforeForDateSequenceTests();
+        ValidationError error = errors.get(2);
+        assertTrue(isEqual(errors.get(2), new ValidationError("agreementDateTo", "Must not be before agreementDateFrom!!!")));
     }
 }
