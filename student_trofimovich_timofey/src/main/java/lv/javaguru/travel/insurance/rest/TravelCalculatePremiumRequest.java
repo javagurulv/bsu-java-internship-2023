09aa523a8dd93f069b2a0bc4996c502256ded165
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2023-11-14 15:48:18 +0300

    STEP5=DONE

--- student_trofimovich_timofey/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
+++ student_trofimovich_timofey/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumRequest.java
@@ -1,29 +1,17 @@
 package lv.javaguru.travel.insurance.rest;
 
-import lombok.Data;
+import lombok.*;
 
 import java.util.Date;
 
-@Data()
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class TravelCalculatePremiumRequest {
 
     private String personFirstName;
     private String personLastName;
     private Date agreementDateFrom;
     private Date agreementDateTo;
-
-
-    public TravelCalculatePremiumRequest() { }
-
-    public TravelCalculatePremiumRequest(String personFirstName,
-                                         String personLastName,
-                                         Date agreementDateFrom,
-                                         Date agreementDateTo) {
-        this.personFirstName = personFirstName;
-        this.personLastName = personLastName;
-        this.agreementDateFrom = agreementDateFrom;
-        this.agreementDateTo = agreementDateTo;
-    }
-
-
 }
