93cfb9e51817c5c6284f7e3bf48ef2e6fd04e927
katierevinska <katerevinska05@gmail.com>
2023-11-16 00:21:51 +0300

    step_18 Done

--- student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,6 +1,7 @@
 package lv.javaguru.travel.insurance.rest;
 
-import org.testng.annotations.Test;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -9,10 +10,14 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.util.ResourceUtils;
 
-import static org.hamcrest.Matchers.is;
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -20,25 +25,28 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @AutoConfigureMockMvc
 public class TravelCalculatePremiumControllerTest {
 
-    @Autowired private MockMvc mockMvc;
+    @Autowired
+    private MockMvc mockMvc;
 
     @Test
     public void simpleRestControllerTest() throws Exception {
-        mockMvc.perform(post("/insurance/travel/")
-                        .content("{" +
-                                "\"personFirstName\" : \"Vasja\",\n" +
-                                "\"personLastName\" : \"Pupkin\",\n" +
-                                "\"agreementDateFrom\" : \"2021-05-25\",\n" +
-                                "\"agreementDateTo\" : \"2021-05-29\"\n" +
-                                "}")
+
+        ObjectMapper mapper = new ObjectMapper();
+        String response = mockMvc.perform(post("/insurance/travel/")
+                        .content(parseJSONIntoString("TravelCalculatePremiumControllerRequest.json"))
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-                .andExpect(status().isOk())
-                .andExpect(jsonPath("personFirstName", is("Vasja")))
-                .andExpect(jsonPath("personLastName", is("Pupkin")))
-                .andExpect(jsonPath("agreementDateFrom", is("2021-05-25")))
-                .andExpect(jsonPath("agreementDateTo", is("2021-05-29")))
-                .andExpect(jsonPath("agreementPrice", is(4)))
-                .andReturn();
+                        .andExpect(status().isOk())
+                        .andReturn().getResponse()
+                        .getContentAsString();
+        assertEquals(mapper.readTree(parseJSONIntoString("TravelCalculatePremiumControllerResponse.json")), mapper.readTree(response));
     }
 
+    private String parseJSONIntoString(String fileName) throws IOException {
+        try {
+            File file = ResourceUtils.getFile("classpath:" + fileName);
+            return new String(Files.readAllBytes(file.toPath()));
+        } catch (IOException e) {
+            throw new IOException("JSON file " + fileName + " can't be read");
+        }
+    }
 }
