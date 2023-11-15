f0df65d2be8ca7ce73ed824e3ae212ffd1af0def
Kreolancer <kreolancer@gmail.com>
2023-11-16 02:35:27 +0300

    step_8 = done

--- student_polyakov_andrey/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
+++ student_polyakov_andrey/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
@@ -16,6 +16,7 @@ public class TravelCalculatePremiumRequestValidatorTest {
         TravelCalculatePremiumRequestValidator reqVal = new TravelCalculatePremiumRequestValidator();
         TravelCalculatePremiumRequest reqMock = mock(TravelCalculatePremiumRequest.class);
         when(reqMock.getPersonFirstName()).thenReturn("");
+        when(reqMock.getPersonLastName()).thenReturn("Pupkin");
         List<ValidationError> errorsList = reqVal.validate(reqMock);
         assertFalse(errorsList.isEmpty());
         assertEquals(errorsList.get(0).getField(), "personFirstName");
@@ -24,21 +25,49 @@ public class TravelCalculatePremiumRequestValidatorTest {
     }
 
     @Test
-    public void fistNameShouldNotBeNull() {
+    public void firstNameShouldNotBeNull() {
         TravelCalculatePremiumRequestValidator reqVal = new TravelCalculatePremiumRequestValidator();
         TravelCalculatePremiumRequest reqMock = mock(TravelCalculatePremiumRequest.class);
         when(reqMock.getPersonFirstName()).thenReturn(null);
+        when(reqMock.getPersonLastName()).thenReturn("Pupkin");
         List<ValidationError> errorsList = reqVal.validate(reqMock);
         assertFalse(errorsList.isEmpty());
         assertEquals(errorsList.get(0).getField(), "personFirstName");
         assertEquals(errorsList.get(0).getMessage(), "Must not be empty!");
         assertEquals(errorsList.size(), 1);
     }
+
     @Test
-    public void errorShouldNotBeCalledWhenFistNameIsPresent() {
+    public void lastNameShouldNotBeEmpty() {
         TravelCalculatePremiumRequestValidator reqVal = new TravelCalculatePremiumRequestValidator();
         TravelCalculatePremiumRequest reqMock = mock(TravelCalculatePremiumRequest.class);
         when(reqMock.getPersonFirstName()).thenReturn("Vasya");
+        when(reqMock.getPersonLastName()).thenReturn("");
+        List<ValidationError> errorsList = reqVal.validate(reqMock);
+        assertFalse(errorsList.isEmpty());
+        assertEquals(errorsList.get(0).getField(), "personLastName");
+        assertEquals(errorsList.get(0).getMessage(), "Must not be empty!");
+        assertEquals(errorsList.size(), 1);
+    }
+
+    @Test
+    public void lastNameShouldNotBeNull() {
+        TravelCalculatePremiumRequestValidator reqVal = new TravelCalculatePremiumRequestValidator();
+        TravelCalculatePremiumRequest reqMock = mock(TravelCalculatePremiumRequest.class);
+        when(reqMock.getPersonFirstName()).thenReturn("Vasya");
+        when(reqMock.getPersonLastName()).thenReturn(null);
+        List<ValidationError> errorsList = reqVal.validate(reqMock);
+        assertFalse(errorsList.isEmpty());
+        assertEquals(errorsList.get(0).getField(), "personLastName");
+        assertEquals(errorsList.get(0).getMessage(), "Must not be empty!");
+        assertEquals(errorsList.size(), 1);
+    }
+    @Test
+    public void errorShouldNotBeCalledWhenFirstLastNameIsPresent() {
+        TravelCalculatePremiumRequestValidator reqVal = new TravelCalculatePremiumRequestValidator();
+        TravelCalculatePremiumRequest reqMock = mock(TravelCalculatePremiumRequest.class);
+        when(reqMock.getPersonFirstName()).thenReturn("Vasya");
+        when(reqMock.getPersonLastName()).thenReturn("Pupkin");
         List<ValidationError> errorsList = reqVal.validate(reqMock);
         assertTrue(errorsList.isEmpty());
     }
