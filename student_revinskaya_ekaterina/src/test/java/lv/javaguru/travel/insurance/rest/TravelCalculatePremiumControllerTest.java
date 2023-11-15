b5d9eeeeb81a0e73b9d36dc3c3da5bbc1f7f06d5
javagurulv <javagurulv@gmail.com>
2023-11-15 08:58:38 +0200

    test fixed

--- student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,6 +1,6 @@
 package lv.javaguru.travel.insurance.rest;
 
-import org.testng.annotations.Test;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
