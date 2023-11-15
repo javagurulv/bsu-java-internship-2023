b8849556c68649ed26564ea6d87dd745fd67e13c
LarionovaAnastasiaAlexandrovna <larionova.anastasia.alexandrovna@gmail.com>
2023-11-15 23:21:54 +0300

    step_3 = DONE

--- student_larionova_anastasia/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_larionova_anastasia/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -4,6 +4,7 @@ import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
 
+import java.math.BigDecimal;
 import java.util.Date;
 import java.util.Objects;
 
@@ -14,13 +15,14 @@ class TravelCalculatePremiumServiceImplTest {
         new TravelCalculatePremiumResponse();
         TravelCalculatePremiumResponse response;
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest(
-                "Ivanov", "Ivan", new Date(200L), new Date(2000L));
+                "Ivanov", "Ivan", new Date(200L), new Date(2000L), new BigDecimal(2));
         TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
         response = service.calculatePremium(request);
         assert (Objects.equals(request.getPersonFirstName(), response.getPersonFirstName())
                 && Objects.equals(request.getPersonLastName(), response.getPersonLastName())
                 && request.getAgreementDateFrom() == response.getAgreementDateFrom()
-                && request.getAgreementDateTo() == response.getAgreementDateTo());
+                && request.getAgreementDateTo() == response.getAgreementDateTo()
+                && Objects.equals(request.getAgreementPrice(), response.getAgreementPrice()));
     }
 
 }
