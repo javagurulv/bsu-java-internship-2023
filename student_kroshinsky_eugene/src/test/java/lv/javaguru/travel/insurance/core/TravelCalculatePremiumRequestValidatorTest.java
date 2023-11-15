98aae03f3d8a05ba83f13d3beca7b2c1d5154c1a
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2023-11-15 14:22:19 +0300

    step_11 = DONE

--- student_kroshinsky_eugene/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
+++ student_kroshinsky_eugene/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumRequestValidatorTest.java
@@ -2,19 +2,14 @@ package lv.javaguru.travel.insurance.core;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.ValidationError;
-import org.apache.coyote.Request;
-import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.boot.context.properties.bind.validation.ValidationErrors;
 
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
-import java.util.Optional;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 public class TravelCalculatePremiumRequestValidatorTest {
     private  TravelCalculatePremiumRequest request;
@@ -22,6 +17,9 @@ public class TravelCalculatePremiumRequestValidatorTest {
     private  List<ValidationError> expectedErrors;
     private  ValidationError validationFirstNameError;
     private  ValidationError validationLastNameError;
+    private  ValidationError validationAgreementDateToError = new ValidationError("agreementDateTo", "Must not be empty!");
+    private  ValidationError validationAgreementDateFromError = new ValidationError("agreementDateFrom", "Must not be empty!");
+    private  ValidationError validationAgreementDateDifference = new ValidationError("agreementDateDifference", "DateTo must be greater than datteFrom");
     @BeforeEach
     private  void init(){
         validator = new TravelCalculatePremiumRequestValidator();
@@ -123,6 +121,49 @@ public class TravelCalculatePremiumRequestValidatorTest {
         );
         expectedErrors.add(validationFirstNameError);
         expectedErrors.add(validationLastNameError);
+        assertEquals(expectedErrors.size(), validator.validate(request).size());
+        for (int i = 0; i < expectedErrors.size(); i++) {
+            assertEquals(expectedErrors.get(i), validator.validate(request).get(i));
+        }
+    }
+    @Test
+    public void validateTestNullDateFromError(){
+        request = new TravelCalculatePremiumRequest(
+                "Eugene",
+                "Kroshinsky",
+                null,
+                new Date(2023, 1, 1)
+        );
+        expectedErrors.add(validationAgreementDateFromError);
+        assertEquals(expectedErrors.size(), validator.validate(request).size());
+        for (int i = 0; i < expectedErrors.size(); i++) {
+            assertEquals(expectedErrors.get(i), validator.validate(request).get(i));
+        }
+    }
+    @Test
+    public void validateTestNullDateToError(){
+        request = new TravelCalculatePremiumRequest(
+                "Eugene",
+                "Kroshinsky",
+                new Date(2023, 1, 1),
+                null
+        );
+        expectedErrors.add(validationAgreementDateToError);
+        assertEquals(expectedErrors.size(), validator.validate(request).size());
+        for (int i = 0; i < expectedErrors.size(); i++) {
+            assertEquals(expectedErrors.get(i), validator.validate(request).get(i));
+        }
+    }
+
+    @Test
+    public void validateTestNullDateDifference(){
+        request = new TravelCalculatePremiumRequest(
+                "Eugene",
+                "Kroshinsky",
+                new Date(2023, 1, 10),
+                new Date(2023, 1, 1)
+        );
+        expectedErrors.add(validationAgreementDateDifference);
         assertEquals(expectedErrors.size(), validator.validate(request).size());
         for (int i = 0; i < expectedErrors.size(); i++) {
             assertEquals(expectedErrors.get(i), validator.validate(request).get(i));
