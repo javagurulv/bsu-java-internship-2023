c00fa7240770ff4d519834b7516fe055ddc2d545
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2023-11-18 12:41:09 +0300

    step_19=DONE

--- student_trofimovich_timofey/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_trofimovich_timofey/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,8 +1,8 @@
 package lv.javaguru.travel.insurance.rest;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.skyscreamer.jsonassert.JSONAssert;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -11,12 +11,8 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 
-import static org.hamcrest.Matchers.is;
-import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -27,15 +23,58 @@ public class TravelCalculatePremiumControllerTest {
     @Autowired private MockMvc mockMvc;
 
     @Autowired private JsonFileReader jsonFileReader;
-    private ObjectMapper mapper = new ObjectMapper();
 
     @Test
-    public void simpleRestControllerTest() throws Exception {
+    public void successfulResponse() throws Exception {
         getResponseAndCompare(
-                "rest/request.json",
-                "rest/response.json"
+                "rest/СorrectRequest.json",
+                "rest/CorrectResponse.json"
         );
     }
+    @Test
+    public void emptyFirstNameError() throws Exception {
+        getResponseAndCompare(
+                "rest/EmptyFirstNameRequest.json",
+                "rest/EmptyFirstNameErrorResponse.json"
+        );
+    }
+    @Test
+    public void emptyLastNameError() throws Exception {
+        getResponseAndCompare(
+                "rest/EmptyLastNameRequest.json",
+                "rest/EmptyLastNameErrorResponse.json"
+        );
+    }
+    @Test
+    public void emptyDateFromError() throws Exception {
+        getResponseAndCompare(
+                "rest/EmptyDateFromRequest.json",
+                "rest/EmptyDateFromErrorResponse.json"
+        );
+    }
+    @Test
+    public void emptyDateToError() throws Exception {
+        getResponseAndCompare(
+                "rest/EmptyDateToRequest.json",
+                "rest/EmptyDateToErrorResponse.json"
+        );
+    }
+    @Test
+    public void dateToLessThanDateFromError() throws Exception {
+        getResponseAndCompare(
+                "rest/DateToIsLessThanDateFromRequest.json",
+                "rest/DateToIsLessThanDateFromErrorResponse.json"
+        );
+    }
+    @Test
+    public void nothingProvided() throws Exception {
+        getResponseAndCompare(
+                "rest/NothingProvidedRequest.json",
+                "rest/NothingProvidedErrorsResponse.json"
+        );
+    }
+
+
 
     private void getResponseAndCompare(String jsonRequestFilePath,
                                    String jsonResponseFilePath) throws Exception {
@@ -51,8 +90,7 @@ public class TravelCalculatePremiumControllerTest {
 
         String jsonResponse = jsonFileReader.readJsonFromFile(jsonResponseFilePath);
 
-        assertEquals(mapper.readTree(responseBodyContent), mapper.readTree(jsonResponse));
+        JSONAssert.assertEquals(responseBodyContent, jsonResponse, false);
     }
 
 }
-
