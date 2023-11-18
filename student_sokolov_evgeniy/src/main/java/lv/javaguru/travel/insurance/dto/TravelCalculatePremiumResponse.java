536fdb5bd5beca0a4f6f3b278123b61b032feb3c
Evgeniy Sokolov <ayanammirei@gmail.com>
2023-11-18 19:25:01 +0300

    step_5-6 = DONE

--- student_sokolov_evgeniy/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_sokolov_evgeniy/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -7,12 +7,13 @@ import lombok.Setter;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TravelCalculatePremiumResponse {
+public class TravelCalculatePremiumResponse extends CoreResponse {
 
     private String personFirstName;
     private String personLastName;
@@ -20,5 +21,9 @@ public class TravelCalculatePremiumResponse {
     private Date agreementDateTo;
 
     private BigDecimal agreementPrice;
+
+    public TravelCalculatePremiumResponse(List<ValidationError> errors) {
+        super(errors);
+    }
 
 }
