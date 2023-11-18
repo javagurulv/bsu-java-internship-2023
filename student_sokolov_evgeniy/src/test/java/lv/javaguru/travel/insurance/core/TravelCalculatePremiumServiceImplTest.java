251e4a1a7d584a9c45bdde731d3ad3fb489683dc
Evgeniy Sokolov <ayanammirei@gmail.com>
2023-11-18 16:41:38 +0300

    step_4 = DONE

--- student_sokolov_evgeniy/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_sokolov_evgeniy/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -1,40 +1,12 @@
 package lv.javaguru.travel.insurance.core;
 
-import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
-import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
 import org.junit.jupiter.api.Test;
-
-import java.math.BigDecimal;
-import java.util.Date;
-import java.time.LocalDate;
-import java.time.ZoneId;
-
-import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class TravelCalculatePremiumServiceImplTest {
 
     @Test
-    public void testCalculatePremium() {
-        TravelCalculatePremiumServiceImpl service = new TravelCalculatePremiumServiceImpl();
+    public void deleteMe() {
 
-        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest();
-        request.setPersonFirstName("John");
-        request.setPersonLastName("Doe");
-
-        Date agreementDateFrom = Date.from(LocalDate.of(2023, 1, 1).atStartOfDay(ZoneId.systemDefault()).toInstant());
-        Date agreementDateTo = Date.from(LocalDate.of(2023, 12, 31).atStartOfDay(ZoneId.systemDefault()).toInstant());
-
-        request.setAgreementDateFrom(agreementDateFrom);
-        request.setAgreementDateTo(agreementDateTo);
-
-        TravelCalculatePremiumResponse response = service.calculatePremium(request);
-
-        assertEquals("John", response.getPersonFirstName());
-        assertEquals("Doe", response.getPersonLastName());
-        assertEquals(null, response.getAgreementPrice());
-
-        BigDecimal expectedAgreementPrice = new BigDecimal("100.50");
-        response.setAgreementPrice(expectedAgreementPrice);
-        assertEquals(expectedAgreementPrice, response.getAgreementPrice());
     }
+
 }
