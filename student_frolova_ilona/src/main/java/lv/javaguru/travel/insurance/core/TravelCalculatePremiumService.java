7413d7898b3f9c531ba5a9f9da82258d9b00a8ca
blackberry020 <ilonafrolova00@gmail.com>
2023-11-18 20:31:24 +0300

    step_14 done

--- student_frolova_ilona/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumService.java
+++ student_frolova_ilona/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumService.java
@@ -2,6 +2,7 @@ package lv.javaguru.travel.insurance.core;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
+import org.springframework.context.annotation.Bean;
 
 public interface TravelCalculatePremiumService {
 
