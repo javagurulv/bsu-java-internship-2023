e1728933312f3b3a3de3c6448599155cde75b089
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2023-11-20 00:09:11 +0300

    step_19 = DONE

--- student_kroshinsky_eugene/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_kroshinsky_eugene/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -11,12 +11,9 @@ import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.ResultActions;
 
-import static org.hamcrest.Matchers.is;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -27,10 +24,66 @@ public class TravelCalculatePremiumControllerTest {
     @Autowired private MockMvc mockMvc;
     @Autowired private JsonFileService reader;
     private ObjectMapper mapper = new ObjectMapper();
+
     @Test
-    public void simpleRestControllerTest() throws Exception {
-        String requestJson = reader.readJsonFile("classpath:rest/TravelCalculatePremiumRequest.json");
-        String responseJson = reader.readJsonFile("classpath:rest/TravelCalculatePremiumResponse.json");
+    public void controllerTestWithoutErrors() throws Exception {
+       simpleRestControllerTest(
+               "rest/TravelCalculatePremiumRequestWithoutErrors.json",
+               "rest/TravelCalculatePremiumResponseWithoutErrors.json"
+       );
+    }
+    @Test
+    public void controllerTestEmptyDateFrom() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestEmptyDateFrom.json",
+                "rest/TravelCalculatePremiumResponseEmptyDateFrom.json"
+        );
+    }
+    @Test
+    public void controllerTestEmptyDateTo() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestEmptyDateTo.json",
+                "rest/TravelCalculatePremiumResponseEmptyDateTo.json"
+        );
+    }
+    @Test
+    public void controllerTestEmptyEmptyFirstName() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestEmptyFirstName.json",
+                "rest/TravelCalculatePremiumResponseEmptyFirstName.json"
+        );
+    }
+    @Test
+    public void controllerTestEmptyEmptyLastName() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestEmptyLastName.json",
+                "rest/TravelCalculatePremiumResponseEmptyLastName.json"
+        );
+    }
+    @Test
+    public void controllerTestEmptyNullFirstName() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestNullFirstName.json",
+                "rest/TravelCalculatePremiumResponseNullFirstName.json"
+        );
+    }
+    @Test
+    public void controllerTestEmptyNullLastName() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestNullLastName.json",
+                "rest/TravelCalculatePremiumResponseNullLastName.json"
+        );
+    }
+    @Test
+    public void controllerTestEmptyWrongDateDifferance() throws Exception {
+        simpleRestControllerTest(
+                "rest/TravelCalculatePremiumRequestWrongDateDifferance.json",
+                "rest/TravelCalculatePremiumResponseWrongDateDifferance.json"
+        );
+    }
+    private void simpleRestControllerTest(String requestPath, String responsePath) throws Exception {
+        String requestJson = reader.readJsonFile(requestPath);
+        String responseJson = reader.readJsonFile(responsePath);
 
         MvcResult result = mockMvc.perform(post("/insurance/travel/")
                         .content(requestJson)
