7ac1d7bdbde2a96aafca357a5459a15a27106cf0
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2023-11-20 21:49:26 +0300

    step_40 = DONE

--- student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/core/valids/DateToValidationRequest.java
+++ student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/core/valids/DateToValidationRequest.java
@@ -1,16 +1,27 @@
 package lv.javaguru.travel.insurance.core.valids;
 
+import lv.javaguru.travel.insurance.core.ErrorCodeValueUtil;
 import lv.javaguru.travel.insurance.validation.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.validation.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 
 @Component
 public class DateToValidationRequest implements TravelRequestValidation{
+    @Autowired
+    private ErrorCodeValueUtil errorCodeUtil;
+
+    @Override
     public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
         return (request.getAgreementDateTo() == null)
-                ? Optional.of(new ValidationError("agreementDateTo", "Must not be empty!"))
+                ? Optional.of(buildError("ERROR_CODE_4"))
                 : Optional.empty();
+    }
+
+    private ValidationError buildError(String errorCode) {
+        String errorDescription = errorCodeUtil.getErrorDescription(errorCode);
+        return new ValidationError(errorCode, errorDescription);
     }
 }
--- student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/core/valids/LastNameValidationRequest.java
+++ student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/core/valids/LastNameValidationRequest.java
@@ -1,7 +1,9 @@
 package lv.javaguru.travel.insurance.core.valids;
 
+import lv.javaguru.travel.insurance.core.ErrorCodeValueUtil;
 import lv.javaguru.travel.insurance.validation.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.validation.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
@@ -9,9 +11,18 @@ import java.util.Optional;
 public
 class LastNameValidationRequest implements TravelRequestValidation
 {
+    @Autowired
+    private ErrorCodeValueUtil errorCodeUtil;
+
+    @Override
     public Optional<ValidationError> execute(TravelCalculatePremiumRequest request) {
         return (request.getPersonLastName() == null || request.getPersonLastName().isEmpty())
-                ? Optional.of(new ValidationError("personLastName", "Must not be empty!"))
+                ? Optional.of(buildError("ERROR_CODE_8"))
                 : Optional.empty();
+    }
+
+    private ValidationError buildError(String errorCode) {
+        String errorDescription = errorCodeUtil.getErrorDescription(errorCode);
+        return new ValidationError(errorCode, errorDescription);
     }
 }
