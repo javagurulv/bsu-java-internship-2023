0a207763de9cc7f6ee274495a3834b823cf3ffb9
Ivan Filon <vanbest27@gmail.com>
2023-11-22 20:57:32 +0300

    step_6 = DONE

--- student_filon_ivan/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_filon_ivan/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -1,17 +1,15 @@
 package lv.javaguru.travel.insurance.core;
 
-import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumRequest;
-import lv.javaguru.travel.insurance.rest.TravelCalculatePremiumResponse;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-import java.util.Date;
-import java.util.concurrent.TimeUnit;
 
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
-
+    @Autowired private DateTimeService dateTimeService = new DateTimeService();
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
@@ -19,7 +17,7 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         response.setPersonFirstName(request.getPersonFirstName());
         response.setAgreementDateFrom(request.getAgreementDateFrom());
         response.setPersonLastName(request.getPersonLastName());
-        long daysBetween = DateTimeService.getDaysBetween(request.getAgreementDateTo(), request.getAgreementDateFrom());
+        long daysBetween = dateTimeService.getDaysBetween(request.getAgreementDateTo(), request.getAgreementDateFrom());
         response.setAgreementPrice(BigDecimal.valueOf(daysBetween));
         return response;
     }
