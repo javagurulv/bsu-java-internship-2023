d77b81014977be223eacdb6d02b799f091000a30
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2023-11-23 22:07:42 +0300

    STEP_40=DONE

--- student_trofimovich_timofey/src/test/java/lv/javaguru/travel/insurance/core/validations/SelectedRisksValidationTest.java
+++ student_trofimovich_timofey/src/test/java/lv/javaguru/travel/insurance/core/validations/SelectedRisksValidationTest.java
@@ -1,8 +1,13 @@
 package lv.javaguru.travel.insurance.core.validations;
 
+import lv.javaguru.travel.insurance.core.ErrorCodeUtil;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
 import java.util.Collections;
 import java.util.List;
@@ -12,26 +17,32 @@ import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
+@ExtendWith(MockitoExtension.class)
 public class SelectedRisksValidationTest {
-    private SelectedRisksValidation validation = new SelectedRisksValidation();
+    @Mock
+    ErrorCodeUtil errorCodeUtil;
+    @InjectMocks
+    private SelectedRisksValidation validation;
     @Test
     void shouldReturnErrorWhenSelectedRisksListIsEmpty() {
         TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
         when(request.getSelectedRisks()).thenReturn(Collections.emptyList());
+        when(errorCodeUtil.getErrorDescription("ERROR_CODE_8")).thenReturn("Selected risks list must not be empty!");
         Optional<ValidationError> validationError = validation.validate(request);
         assertThat(validationError).isPresent();
-        assertThat(validationError.get().getField()).isEqualTo("selectedRisks");
-        assertThat(validationError.get().getMessage()).isEqualTo("Must not be empty!");
+        assertThat(validationError.get().getErrorCode()).isEqualTo("ERROR_CODE_8");
+        assertThat(validationError.get().getDescription()).isEqualTo("Selected risks list must not be empty!");
     }
 
     @Test
     void shouldReturnErrorWhenSelectedRisksListIsNull() {
         TravelCalculatePremiumRequest request = mock(TravelCalculatePremiumRequest.class);
         when(request.getSelectedRisks()).thenReturn(null);
+        when(errorCodeUtil.getErrorDescription("ERROR_CODE_8")).thenReturn("Selected risks list must not be empty!");
         Optional<ValidationError> validationError = validation.validate(request);
         assertThat(validationError).isPresent();
-        assertThat(validationError.get().getField()).isEqualTo("selectedRisks");
-        assertThat(validationError.get().getMessage()).isEqualTo("Must not be empty!");
+        assertThat(validationError.get().getErrorCode()).isEqualTo("ERROR_CODE_8");
+        assertThat(validationError.get().getDescription()).isEqualTo("Selected risks list must not be empty!");
     }
 
     @Test
