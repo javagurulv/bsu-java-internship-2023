560642317aa225262a77cb0bceb5a9aea12af72d
Denis Lebedev <3d.lebedzeu@gmail.com>
2023-11-24 23:26:04 +0300

    step_12 = DONE

--- student_lebedev_denis/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_lebedev_denis/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -11,20 +11,33 @@ import java.util.List;
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
-    @Autowired private TravelCalculatePremiumRequestValidator requestValidator;
-    @Autowired private DateTimeService dateTimeService;
+    private final TravelCalculatePremiumRequestValidator requestValidator;
+    private final DateTimeService dateTimeService;
 
-    @Override
-    public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
-        List<ValidationError> errors = requestValidator.validate(request);
+    @Autowired
+    public TravelCalculatePremiumServiceImpl(TravelCalculatePremiumRequestValidator requestValidator,
+                                             DateTimeService dateTimeService) {
+        this.requestValidator = requestValidator;
+        this.dateTimeService = dateTimeService;
+    }
+
+    private TravelCalculatePremiumResponse buildResponse(TravelCalculatePremiumRequest request, List<ValidationError> errors) {
         if (!errors.isEmpty()) {
             return new TravelCalculatePremiumResponse(errors);
         }
 
-        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse(
-                request.getPersonFirstName(), request.getPersonLastName(),
-                request.getAgreementDateFrom(), request.getAgreementDateTo());
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
+        response.setPersonFirstName(request.getPersonFirstName());
+        response.setPersonLastName(request.getPersonLastName());
+        response.setAgreementDateFrom(request.getAgreementDateFrom());
+        response.setAgreementDateTo(request.getAgreementDateTo());
         response.setAgreementPrice(new BigDecimal(dateTimeService.getDays(request.getAgreementDateFrom(), request.getAgreementDateTo())));
         return response;
+    }
+
+    @Override
+    public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = requestValidator.validate(request);
+        return buildResponse(request, errors);
     }
 }
