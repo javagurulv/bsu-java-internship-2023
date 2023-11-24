098fae85387a424b6567a3abdcabb89092d871f7
katierevinska <katerevinska05@gmail.com>
2023-11-24 20:49:08 +0300

    step-60 Done

--- student_revinskaya_ekaterina/src/main/java/lv/javaguru/travel/insurance/core/underwriting/SelectedRisksPremiumCalculator.java
+++ student_revinskaya_ekaterina/src/main/java/lv/javaguru/travel/insurance/core/underwriting/SelectedRisksPremiumCalculator.java
@@ -6,15 +6,24 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.List;
+import java.util.Optional;
+
 @Component
 class SelectedRisksPremiumCalculator {
     @Autowired
     private List<TravelRiskPremiumCalculator> riskPremiumCalculators;
 
-    List<TravelRisk> calculateSelectedRisksPremium(TravelCalculatePremiumRequest request){
-        return riskPremiumCalculators.stream()
-                .filter(calculator->request.getSelected_risks().contains(calculator.getRiskIc()))
-                .map(calculator->new TravelRisk(calculator.getRiskIc(),calculator.calculatePremium(request)))
+    List<TravelRisk> calculateSelectedRisksPremium(TravelCalculatePremiumRequest request) {
+        return request.getSelected_risks().stream()
+                .map(this::getCalculatorByIc)
+                .map(calculator -> new TravelRisk(calculator.getRiskIc(), calculator.calculatePremium(request)))
                 .toList();
+    }
+
+    private TravelRiskPremiumCalculator getCalculatorByIc(String riskIc) {
+        return riskPremiumCalculators.stream()
+                .filter(calculator -> calculator.getRiskIc().equals(riskIc))
+                .findFirst()
+                .orElseThrow(() -> new RuntimeException("risk with riskIc " + riskIc + " not supported by system"));
     }
 }
--- student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/core/underwriting/SelectedRisksPremiumCalculatorTest.java
+++ student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/core/underwriting/SelectedRisksPremiumCalculatorTest.java
@@ -13,6 +13,7 @@ import java.math.BigDecimal;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
@@ -22,21 +23,40 @@ public class SelectedRisksPremiumCalculatorTest {
     SelectedRisksPremiumCalculator selectedRisksPremiumCalculator;
     @Mock
     TravelCalculatePremiumRequest request;
+@Test
+public void calculateWithNotExistSelectedRisksPremium(){
+    when(request.getSelected_risks()).thenReturn(List.of("RISK_1", "RISK_2"));
+
+    TravelRiskPremiumCalculator calculator1 = mock(TravelRiskPremiumCalculator.class);
+    when(calculator1.getRiskIc()).thenReturn("RISK_1");
+    when(calculator1.calculatePremium(request)).thenReturn(BigDecimal.valueOf(2));
+
+    List<TravelRiskPremiumCalculator> riskPremiumCalculators = List.of(calculator1);
+    ReflectionTestUtils.setField(selectedRisksPremiumCalculator, "riskPremiumCalculators", riskPremiumCalculators);
+    Throwable thrown = assertThrows(RuntimeException.class, ()->selectedRisksPremiumCalculator.calculateSelectedRisksPremium(request));
+
+    assertEquals(thrown.getMessage(), "risk with riskIc RISK_2 not supported by system");
+}
     @Test
     public void calculateSelectedRisksPremium(){
         when(request.getSelected_risks()).thenReturn(List.of("RISK_1", "RISK_2"));
+
         TravelRiskPremiumCalculator calculator1 = mock(TravelRiskPremiumCalculator.class);
         when(calculator1.getRiskIc()).thenReturn("RISK_1");
         when(calculator1.calculatePremium(request)).thenReturn(BigDecimal.valueOf(2));
+
         TravelRiskPremiumCalculator calculator2 = mock(TravelRiskPremiumCalculator.class);
         when(calculator2.getRiskIc()).thenReturn("RISK_2");
         when(calculator2.calculatePremium(request)).thenReturn(BigDecimal.valueOf(3));
+
         List<TravelRiskPremiumCalculator> riskPremiumCalculators = List.of(calculator1, calculator2);
         ReflectionTestUtils.setField(selectedRisksPremiumCalculator, "riskPremiumCalculators", riskPremiumCalculators);
         List<TravelRisk> risks = selectedRisksPremiumCalculator.calculateSelectedRisksPremium(request);
         assertEquals(risks.size(), 2);
+
         assertEquals(risks.get(0).getRiskIc(), "RISK_1");
         assertEquals(risks.get(0).getPremium(), BigDecimal.valueOf(2));
+
         assertEquals(risks.get(1).getRiskIc(), "RISK_2");
         assertEquals(risks.get(1).getPremium(), BigDecimal.valueOf(3));
 
