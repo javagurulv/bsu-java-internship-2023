d6c41f6aca11c098a2e8c373e50b3a0d38c22058
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2023-11-25 17:01:12 +0300

    STEP 76 = DONE

--- student_kalesnik_ivan/DB/DB_dataScript.sql
+++ student_kalesnik_ivan/DB/DB_dataScript.sql
@@ -5,22 +5,73 @@ INSERT INTO classifier_values(
     classifier_id,
     ic,
     description)
-SELECT cl.id, tmp.ic, tmp.description
+SELECT
+    cl.id,
+    'TRAVEL_MEDICAL',
+    'Travel policy medical risk type'
 FROM classifiers as cl
-         CROSS JOIN (
-    SELECT 'TRAVEL_MEDICAL' AS ic, 'Travel policy medical risk type' AS description
-    UNION ALL
-    SELECT 'TRAVEL_CANCELLATION', 'Travel policy trip cancellation risk type'
-    UNION ALL
-    SELECT 'TRAVEL_LOSS_BAGGAGE', 'Travel policy baggage lose risk type'
-    UNION ALL
-    SELECT 'TRAVEL_THIRD_PARTY_LIABILITY', 'Travel policy third party liability risk type'
-    UNION ALL
-    SELECT 'TRAVEL_EVACUATION', 'Travel policy evacuation risk type'
-    UNION ALL
-    SELECT 'TRAVEL_SPORT_ACTIVITIES', 'Travel policy sport activities risk type'
-) AS tmp
 WHERE cl.title = 'RISK_TYPE';
+
+
+INSERT INTO classifier_values(
+    classifier_id,
+    ic,
+    description)
+SELECT
+    cl.id,
+    'TRAVEL_CANCELLATION',
+    'Travel policy trip cancellation risk type'
+FROM classifiers as cl
+WHERE cl.title = 'RISK_TYPE';
+
+
+INSERT INTO classifier_values(
+    classifier_id,
+    ic,
+    description)
+SELECT
+    cl.id,
+    'TRAVEL_LOSS_BAGGAGE',
+    'Travel policy baggage lose risk type'
+FROM classifiers as cl
+WHERE cl.title = 'RISK_TYPE';
+
+
+INSERT INTO classifier_values(
+    classifier_id,
+    ic,
+    description)
+SELECT
+    cl.id,
+    'TRAVEL_THIRD_PARTY_LIABILITY',
+    'Travel policy third party liability risk type'
+FROM classifiers as cl
+WHERE cl.title = 'RISK_TYPE';
+
+
+INSERT INTO classifier_values(
+    classifier_id,
+    ic,
+    description)
+SELECT
+    cl.id,
+    'TRAVEL_EVACUATION',
+    'Travel policy evacuation risk type'
+FROM classifiers as cl
+WHERE cl.title = 'RISK_TYPE';
+
+
+INSERT INTO classifier_values(
+    classifier_id,
+    ic,
+    description)
+SELECT
+    cl.id,
+    'TRAVEL_SPORT_ACTIVITIES',
+    'Travel policy sport activities risk type'
+FROM classifiers as cl
+WHERE cl.title = 'RISK_TYPE';
+
 
 INSERT INTO classifiers(title, description)
 VALUES('COUNTRY', 'Country classifier');
@@ -58,6 +109,8 @@ SELECT
 FROM classifiers as cl
 WHERE cl.title = 'COUNTRY';
 
+
+
 INSERT INTO country_default_day_rate(country_ic, default_day_rate)
 VALUES('LATVIA', 1.00);
 
@@ -66,6 +119,7 @@ VALUES('SPAIN', 2.50);
 
 INSERT INTO country_default_day_rate(country_ic, default_day_rate)
 VALUES('JAPAN', 3.50);
+
 
 INSERT INTO age_coefficient(age_from, age_to, coefficient)
 VALUES(0, 5, 1.1);
@@ -84,6 +138,7 @@ VALUES(41, 65, 1.2);
 
 INSERT INTO age_coefficient(age_from, age_to, coefficient)
 VALUES(65, 150, 1.5);
+
 
 INSERT INTO classifiers(title, description)
 VALUES('MEDICAL_RISK_LIMIT_LEVEL', 'Medical Risk limit level classifier');
@@ -133,3 +188,16 @@ SELECT
 FROM classifiers as cl
 WHERE cl.title = 'MEDICAL_RISK_LIMIT_LEVEL';
 
+
+
+INSERT INTO medical_risk_limit_level(medical_risk_limit_level_ic, coefficient)
+VALUES('LEVEL_10000', 1.0);
+
+INSERT INTO medical_risk_limit_level(medical_risk_limit_level_ic, coefficient)
+VALUES('LEVEL_15000', 1.2);
+
+INSERT INTO medical_risk_limit_level(medical_risk_limit_level_ic, coefficient)
+VALUES('LEVEL_20000', 1.5);
+
+INSERT INTO medical_risk_limit_level(medical_risk_limit_level_ic, coefficient)
+VALUES('LEVEL_50000', 2.0);
