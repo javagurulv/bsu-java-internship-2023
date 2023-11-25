5485ffd45e1a16ea187193df74340e214f1aa2e8
Cheglockvvv <cheglock.vvv@gmail.com>
2023-11-25 22:05:19 +0300

    step_22 = DONE

--- student_vorobey_artyom/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImpl.java
+++ student_vorobey_artyom/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImpl.java
@@ -1,23 +1,21 @@
-package lv.javaguru.travel.insurance.core;
+package lv.javaguru.travel.insurance.core.validations;
 
-import java.util.ArrayList;
-import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-import lv.javaguru.travel.insurance.core.validations.TravelRequestValidation;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 @Component
-public class TravelCalculatePremiumRequestValidator {
+public class TravelCalculatePremiumRequestValidatorImpl implements TravelCalculatePremiumRequestValidator{
 
     @Autowired
     List<TravelRequestValidation> requestValidations;
 
+    @Override
     public List<ValidationError> validate (TravelCalculatePremiumRequest request) {
         return requestValidations.stream()
                 .map(validator -> validator.validateArgs(request))
