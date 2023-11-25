0757ef1cd295a23aeddd7104c81467456f445a71
katierevinska <katerevinska05@gmail.com>
2023-11-25 17:25:58 +0300

    step_68 done

--- student_revinskaya_ekaterina/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelRequestBirthdayValidation.java
+++ student_revinskaya_ekaterina/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelRequestBirthdayValidation.java
@@ -2,19 +2,20 @@ package lv.javaguru.travel.insurance.core.validations;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.checkerframework.checker.units.qual.C;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.Optional;
 @Component
-class TravelRequestSelectedRisksValidation extends TravelRequestValidationImpl {
+public class TravelRequestBirthdayValidation extends TravelRequestValidationImpl {
     @Autowired
     private ValidationErrorFactory validationErrorFactory;
+
     @Override
     public Optional<ValidationError> validate(TravelCalculatePremiumRequest request) {
-        return (request.getSelectedRisks() == null || request.getSelectedRisks().isEmpty())
-                ? Optional.of(validationErrorFactory.buildError("ERROR_CODE_8"))
+        return request.getBirthday() == null
+                ? Optional.of(validationErrorFactory.buildError("ERROR_CODE_12"))
                 : Optional.empty();
     }
-
 }
