200ada965df883ffd747d465fc35b30784149ef1
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2023-11-26 19:38:27 +0300

    STEP 85 = DONE

--- student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -14,7 +14,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/insurance/travel")
+@RequestMapping("/insurance/travel/api")
 class TravelCalculatePremiumController {
 
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
