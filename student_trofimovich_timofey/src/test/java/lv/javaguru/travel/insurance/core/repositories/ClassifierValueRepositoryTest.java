dc317090ea1a2b3a05bd4cca718a4b8fe570f63b
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2023-11-26 20:14:42 +0300

    STEP_49=DONE

--- student_trofimovich_timofey/src/test/java/lv/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
+++ student_trofimovich_timofey/src/test/java/lv/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
@@ -24,63 +24,46 @@ public class ClassifierValueRepositoryTest {
 
     @Test
     public void should_find_RiskType_TRAVEL_MEDICAL() {
-        Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
-                .findByClassifierTitleAndIc("RISK_TYPE", "TRAVEL_MEDICAL");
-        assertThat(optionalClassifierValue).isPresent();
-        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
-        assertThat(optionalClassifierValue.get().getIc()).isEqualTo("TRAVEL_MEDICAL");
-
+       checkIfRiskTypeWithGivenIcExists("TRAVEL_MEDICAL");
     }
 
     @Test
-    public void should_find_RiskType_TRAVEL_CANCELLATON() {
-        Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
-                .findByClassifierTitleAndIc("RISK_TYPE", "TRAVEL_CANCELLATION");
-        assertThat(optionalClassifierValue).isPresent();
-        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
-        assertThat(optionalClassifierValue.get().getIc()).isEqualTo("TRAVEL_CANCELLATION");
+    public void should_find_RiskType_TRAVEL_CANCELLATION() {
+        checkIfRiskTypeWithGivenIcExists("TRAVEL_CANCELLATION");
 
     }
 
     @Test
     public void should_find_RiskType_TRAVEL_LOSS_BAGGAGE() {
-        Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
-                .findByClassifierTitleAndIc("RISK_TYPE", "TRAVEL_LOSS_BAGGAGE");
-        assertThat(optionalClassifierValue).isPresent();
-        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
-        assertThat(optionalClassifierValue.get().getIc()).isEqualTo("TRAVEL_LOSS_BAGGAGE");
+        checkIfRiskTypeWithGivenIcExists("TRAVEL_LOSS_BAGGAGE");
 
     }
 
     @Test
     public void should_find_RiskType_TRAVEL_THIRD_PARTY_LIABILITY() {
-        Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
-                .findByClassifierTitleAndIc("RISK_TYPE", "TRAVEL_THIRD_PARTY_LIABILITY");
-        assertThat(optionalClassifierValue).isPresent();
-        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
-        assertThat(optionalClassifierValue.get().getIc()).isEqualTo("TRAVEL_THIRD_PARTY_LIABILITY");
+        checkIfRiskTypeWithGivenIcExists("TRAVEL_THIRD_PARTY_LIABILITY");
     }
 
     @Test
     public void should_find_RiskType_TRAVEL_EVACUATION() {
-        Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
-                .findByClassifierTitleAndIc("RISK_TYPE", "TRAVEL_EVACUATION");
-        assertThat(optionalClassifierValue).isPresent();
-        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
-        assertThat(optionalClassifierValue.get().getIc()).isEqualTo("TRAVEL_EVACUATION");
+        checkIfRiskTypeWithGivenIcExists("TRAVEL_EVACUATION");
     }
     @Test
     public void should_find_RiskType_TRAVEL_SPORT_ACTIVITIES() {
-        Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
-                .findByClassifierTitleAndIc("RISK_TYPE", "TRAVEL_SPORT_ACTIVITIES");
-        assertThat(optionalClassifierValue).isPresent();
-        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
-        assertThat(optionalClassifierValue.get().getIc()).isEqualTo("TRAVEL_SPORT_ACTIVITIES");
+        checkIfRiskTypeWithGivenIcExists("TRAVEL_SPORT_ACTIVITIES");
     }
     @Test
     public void shouldNotFindRiskType_FAKE() {
         Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
                 .findByClassifierTitleAndIc("RISK_TYPE", "DUMMY");
         assertThat(optionalClassifierValue).isEmpty();
+    }
+
+    private void checkIfRiskTypeWithGivenIcExists(String ic) {
+       Optional<ClassifierValue> optionalClassifierValue = classifierValueRepository
+                .findByClassifierTitleAndIc("RISK_TYPE", ic);
+        assertThat(optionalClassifierValue).isPresent();
+        assertThat(optionalClassifierValue.get().getClassifier().getTitle()).isEqualTo("RISK_TYPE");
+        assertThat(optionalClassifierValue.get().getIc()).isEqualTo(ic);
     }
 }
