0522d8d74db6b2330fbc6945e93bd33003ac6641
blackberry020 <ilonafrolova00@gmail.com>
2023-11-26 15:05:04 +0300

    fixed tests

--- student_frolova_ilona/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_frolova_ilona/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -25,101 +25,101 @@ public class TravelCalculatePremiumControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-//    @Test
-//    public void simpleRestControllerTestExample() throws Exception {
-//        mockMvc.perform(post("/insurance/travel/")
-//                        .content("""
-//                                {"personFirstName" : "Vasja",
-//                                "personLastName" : "Pupkin",
-//                                "agreementDateFrom" : "2029-05-25",
-//                                "agreementDateTo" : "2029-05-29"
-//                                }""")
-//                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("personFirstName", is("Vasja")))
-//                .andExpect(jsonPath("personLastName", is("Pupkin")))
-//                .andExpect(jsonPath("agreementDateFrom", is("2029-05-25")))
-//                .andExpect(jsonPath("agreementDateTo", is("2029-05-29")))
-//                .andExpect(jsonPath("agreementPrice", is(4)))
-//                .andReturn();
-//    }
-//
-//    @Test
-//    public void simpleRestControllerTest() throws Exception {
-//        mockMvc.perform(post("/insurance/travel/")
-//                        .content("""
-//                                {"personFirstName" : "Name",
-//                                "personLastName" : "Surname",
-//                                "agreementDateFrom" : "2029-05-20",
-//                                "agreementDateTo" : "2029-05-29"
-//                                }""")
-//                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-//                .andExpect(status().isOk())
-//                .andExpect(jsonPath("personFirstName", is("Name")))
-//                .andExpect(jsonPath("personLastName", is("Surname")))
-//                .andExpect(jsonPath("agreementDateFrom", is("2029-05-20")))
-//                .andExpect(jsonPath("agreementDateTo", is("2029-05-29")))
-//                .andExpect(jsonPath("agreementPrice", is(9)))
-//                .andReturn();
-//    }
-//
-//    @Test
-//    public void jsonFilesTest() throws Exception {
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_correct.json",
-//                "rest/TravelCalculatePremiumResponse_correct.json"
-//        );
-//
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_allWrong.json",
-//                "rest/TravelCalculatePremiumResponse_allWrong.json"
-//        );
-//
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_dateFromEmpty.json",
-//                "rest/TravelCalculatePremiumResponse_dateFromEmpty.json"
-//        );
-//
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_dateToEmpty.json",
-//                "rest/TravelCalculatePremiumResponse_dateToEmpty.json"
-//        );
-//
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_firstNameEmpty.json",
-//                "rest/TravelCalculatePremiumResponse_firstNameEmpty.json"
-//        );
-//
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_lastNameEmpty.json",
-//                "rest/TravelCalculatePremiumResponse_lastNameEmpty.json"
-//        );
-//
-//        compareResponseToRequestInJsonFiles(
-//                "rest/TravelCalculatePremiumRequest_dateSeq.json",
-//                "rest/TravelCalculatePremiumResponse_dateSeq.json"
-//        );
-//    }
+    @Test
+    public void simpleRestControllerTestExample() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("""
+                                {"personFirstName" : "Vasja",
+                                "personLastName" : "Pupkin",
+                                "agreementDateFrom" : "2029-05-25",
+                                "agreementDateTo" : "2029-05-29"
+                                }""")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is("Vasja")))
+                .andExpect(jsonPath("personLastName", is("Pupkin")))
+                .andExpect(jsonPath("agreementDateFrom", is("2029-05-25")))
+                .andExpect(jsonPath("agreementDateTo", is("2029-05-29")))
+                .andExpect(jsonPath("agreementPrice", is("4.00000")))
+                .andReturn();
+    }
 
-//    public void compareResponseToRequestInJsonFiles(
-//            String fileNameRequest, String fileNameResponse
-//    ) throws Exception {
-//        String jsonRequest = JsonReader.read(fileNameRequest);
-//
-//        MvcResult result = mockMvc.perform(post("/insurance/travel/")
-//                        .content(jsonRequest)
-//                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
-//                .andExpect(status().isOk())
-//                .andReturn();
-//
-//        String responseBodyContent = result.getResponse().getContentAsString();
-//
-//        String jsonResponse = JsonReader.read(fileNameResponse);
-//
-//        assertJson(responseBodyContent)
-//                .where()
-//                .keysInAnyOrder()
-//                .arrayInAnyOrder()
-//                .isEqualTo(jsonResponse);
-//    }
+    @Test
+    public void simpleRestControllerTest() throws Exception {
+        mockMvc.perform(post("/insurance/travel/")
+                        .content("""
+                                {"personFirstName" : "Name",
+                                "personLastName" : "Surname",
+                                "agreementDateFrom" : "2029-05-20",
+                                "agreementDateTo" : "2029-05-29"
+                                }""")
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("personFirstName", is("Name")))
+                .andExpect(jsonPath("personLastName", is("Surname")))
+                .andExpect(jsonPath("agreementDateFrom", is("2029-05-20")))
+                .andExpect(jsonPath("agreementDateTo", is("2029-05-29")))
+                .andExpect(jsonPath("agreementPrice", is("9.00000")))
+                .andReturn();
+    }
+
+    @Test
+    public void jsonFilesTest() throws Exception {
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_correct.json",
+                "rest/TravelCalculatePremiumResponse_correct.json"
+        );
+
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_allWrong.json",
+                "rest/TravelCalculatePremiumResponse_allWrong.json"
+        );
+
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_dateFromEmpty.json",
+                "rest/TravelCalculatePremiumResponse_dateFromEmpty.json"
+        );
+
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_dateToEmpty.json",
+                "rest/TravelCalculatePremiumResponse_dateToEmpty.json"
+        );
+
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_firstNameEmpty.json",
+                "rest/TravelCalculatePremiumResponse_firstNameEmpty.json"
+        );
+
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_lastNameEmpty.json",
+                "rest/TravelCalculatePremiumResponse_lastNameEmpty.json"
+        );
+
+        compareResponseToRequestInJsonFiles(
+                "rest/TravelCalculatePremiumRequest_dateSeq.json",
+                "rest/TravelCalculatePremiumResponse_dateSeq.json"
+        );
+    }
+
+    public void compareResponseToRequestInJsonFiles(
+            String fileNameRequest, String fileNameResponse
+    ) throws Exception {
+        String jsonRequest = JsonReader.read(fileNameRequest);
+
+        MvcResult result = mockMvc.perform(post("/insurance/travel/")
+                        .content(jsonRequest)
+                        .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
+                .andExpect(status().isOk())
+                .andReturn();
+
+        String responseBodyContent = result.getResponse().getContentAsString();
+
+        String jsonResponse = JsonReader.read(fileNameResponse);
+
+        assertJson(responseBodyContent)
+                .where()
+                .keysInAnyOrder()
+                .arrayInAnyOrder()
+                .isEqualTo(jsonResponse);
+    }
 }
