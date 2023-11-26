7452048307fd4f1600e16ddca3838b7808a8a328
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2023-11-26 22:57:59 +0300

    STEP 85 = DONE

--- student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_kalesnik_ivan/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -2,11 +2,11 @@ package lv.javaguru.travel.insurance.rest;
 
 import com.google.common.base.Stopwatch;
 import lv.javaguru.travel.insurance.core.services.TravelCalculatePremiumService;
-import lv.javaguru.travel.insurance.validation.TravelCalculatePremiumRequest;
-import lv.javaguru.travel.insurance.validation.TravelCalculatePremiumResponse;
+import lv.javaguru.travel.insurance.logger.TravelCalculatePremiumRequestExecutionTimeLogger;
 import lv.javaguru.travel.insurance.logger.TravelCalculatePremiumRequestLogger;
 import lv.javaguru.travel.insurance.logger.TravelCalculatePremiumResponseLogger;
-import lv.javaguru.travel.insurance.logger.TravelCalculatePremiumRequestExecutionTimeLogger;
+import lv.javaguru.travel.insurance.validation.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.validation.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -15,22 +15,27 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/insurance/travel/api")
-class TravelCalculatePremiumController {
+public class TravelCalculatePremiumController {
 
+	@Autowired private TravelCalculatePremiumRequestLogger requestLogger;
+	@Autowired private TravelCalculatePremiumResponseLogger responseLogger;
+	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger executionTimeLogger;
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
-	@Autowired private TravelCalculatePremiumRequestLogger loggerForRequest;
-	@Autowired private TravelCalculatePremiumResponseLogger loggerForResponse;
-	@Autowired private TravelCalculatePremiumRequestExecutionTimeLogger requestExecutionTimeLogger;
+
 	@PostMapping(path = "/",
 			consumes = "application/json",
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
-		loggerForRequest.log(request);
 		Stopwatch stopwatch = Stopwatch.createStarted();
+		TravelCalculatePremiumResponse response = processRequest(request);
+		executionTimeLogger.logExecutionTime(stopwatch);
+		return response;
+	}
+
+	private TravelCalculatePremiumResponse processRequest(TravelCalculatePremiumRequest request) {
+		requestLogger.log(request);
 		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
-		//stopwatch.stop();
-		loggerForResponse.log(response);
-		requestExecutionTimeLogger.logExecutionTime(stopwatch);
+		responseLogger.log(response);
 		return response;
 	}
 
