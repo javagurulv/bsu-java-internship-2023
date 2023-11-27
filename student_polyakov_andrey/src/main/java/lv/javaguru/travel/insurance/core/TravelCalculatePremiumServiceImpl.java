8be9431aebe0bb5c8f368fe98bbd30c17c86de87
Kreolancer <kreolancer@gmail.com>
2023-11-28 01:46:39 +0300

    step_12 = done

--- student_polyakov_andrey/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_polyakov_andrey/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -2,15 +2,37 @@ package lv.javaguru.travel.insurance.core;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
-    private DateTimeService dateTimeService;
+
+    private final TravelCalculatePremiumRequestValidator requestValidator;
+
+    private final DateTimeService dateTimeService;
+
+    @Autowired
+    public TravelCalculatePremiumServiceImpl (TravelCalculatePremiumRequestValidator requestValidator, DateTimeService dateTimeService) {
+        this.requestValidator = requestValidator;
+        this.dateTimeService = dateTimeService;
+    }
+
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = requestValidator.validate(request);
+        return createResponse(request, errors);
+    }
+
+    public TravelCalculatePremiumResponse createResponse(TravelCalculatePremiumRequest request, List<ValidationError> errors) {
+        if (!errors.isEmpty()) {
+            return new TravelCalculatePremiumResponse(errors);
+        }
+
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
         response.setPersonFirstName(request.getPersonFirstName());
         response.setPersonLastName(request.getPersonLastName());
