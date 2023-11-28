b4bdc16542e4c5c26eaa458d81a647ea1b2a8d7c
Ivan Filon <vanbest27@gmail.com>
2023-11-28 19:56:15 +0300

    step_12 = DONE

--- student_filon_ivan/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_filon_ivan/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -2,16 +2,33 @@ package lv.javaguru.travel.insurance.core;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
+import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
-
+import java.util.List;
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
-    @Autowired private DateTimeService dateTimeService = new DateTimeService();
+    private final DateTimeService dateTimeService;
+    private final TravelCalculatePremiumRequestValidator validator;
+
+    @Autowired
+    public TravelCalculatePremiumServiceImpl(TravelCalculatePremiumRequestValidator validator, DateTimeService dateTimeService){
+        this.dateTimeService = dateTimeService;
+        this.validator = validator;
+    }
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> list = validator.validate(request);
+        return buildResponse(request, list);
+    }
+
+    public TravelCalculatePremiumResponse buildResponse(TravelCalculatePremiumRequest request, List<ValidationError> list){
+        if(!list.isEmpty()) {
+            return new TravelCalculatePremiumResponse(list);
+        }
+
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse();
         response.setAgreementDateTo(request.getAgreementDateTo());
         response.setPersonFirstName(request.getPersonFirstName());
@@ -19,7 +36,7 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         response.setPersonLastName(request.getPersonLastName());
         long daysBetween = dateTimeService.getDaysBetween(request.getAgreementDateTo(), request.getAgreementDateFrom());
         response.setAgreementPrice(BigDecimal.valueOf(daysBetween));
+
         return response;
     }
-
 }
