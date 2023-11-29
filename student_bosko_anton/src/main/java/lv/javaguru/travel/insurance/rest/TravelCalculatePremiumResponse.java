ef4cb748910fab5172c8cd15fd39e9bf0d5a01c4
funkyfall <funkaa15@gmail.com>
2023-11-29 19:41:06 +0300

    STEP 4 = DONE

--- student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
+++ student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumResponse.java
@@ -2,6 +2,8 @@ package lv.javaguru.travel.insurance.rest;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.Objects;
+import java.util.concurrent.TimeUnit;
 
 public class TravelCalculatePremiumResponse {
 
@@ -19,6 +21,25 @@ public class TravelCalculatePremiumResponse {
         this.setPersonLastName(request.getPersonLastName());
         this.setAgreementDateFrom(request.getAgreementDateFrom());
         this.setAgreementDateTo(request.getAgreementDateTo());
+        this.setAgreementPrice(BigDecimal.valueOf(TimeUnit.DAYS.convert(agreementDateTo.getTime() - agreementDateFrom.getTime(),
+                TimeUnit.MILLISECONDS)));
+    }
+
+    @Override
+    public boolean equals(Object obj)
+    {
+        if(obj == null || this.getClass() != obj.getClass())
+            return false;
+        else if(obj == this)
+            return true;
+        else {
+            TravelCalculatePremiumResponse temp = (TravelCalculatePremiumResponse) obj;
+            return Objects.equals(personFirstName, temp.getPersonFirstName()) &&
+                    Objects.equals(personLastName, temp.getPersonLastName()) &&
+                    agreementDateFrom == temp.getAgreementDateFrom() &&
+                    agreementDateTo == temp.getAgreementDateTo() &&
+                    Objects.equals(agreementPrice, temp.getAgreementPrice());
+        }
     }
 
     public String getPersonFirstName() {
