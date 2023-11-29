124dcbc4aac78ea78e2db577ba8a309ab190ea76
funkyfall <funkaa15@gmail.com>
2023-11-29 21:40:16 +0300

    STEP 6 = DONE

--- student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/dto/CoreResponse.java
+++ student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/dto/CoreResponse.java
@@ -13,5 +13,5 @@ import java.util.List;
 @NoArgsConstructor
 public class CoreResponse {
     private List<ValidationError> errors;
-    public boolean hasErrors(List<ValidationError> err) {return errors != null && !errors.isEmpty();}
+    public boolean hasErrors(List<ValidationError> errors) {return errors != null && !errors.isEmpty();}
 }
