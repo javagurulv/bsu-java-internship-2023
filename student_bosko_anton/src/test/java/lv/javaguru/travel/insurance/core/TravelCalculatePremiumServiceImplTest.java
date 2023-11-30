104beacdcda87c16f70022c69e3dda5254ea8c29
funkyfall <funkaa15@gmail.com>
2023-11-30 22:11:08 +0300

    STEP 12 = DONE

--- student_bosko_anton/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
+++ student_bosko_anton/src/test/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImplTest.java
@@ -6,11 +6,19 @@ import org.junit.jupiter.api.Test;
 
 class TravelCalculatePremiumServiceImplTest {
     DateTimeService date = new DateTimeService();
+
     @Test
-    public void test()
-    {
+    public void test_first() {
         TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("Anton",
                 "Bosko", date.createDate("15.08.2004"), date.createDate("15.08.2005"));
+        TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse(request);
+        TravelCalculatePremiumServiceImpl impl = new TravelCalculatePremiumServiceImpl();
+        assert (response.equals(impl.calculatePremium(request)));
+    }
+    @Test
+    public void test_second() {
+        TravelCalculatePremiumRequest request = new TravelCalculatePremiumRequest("Ludmila",
+                "Velitskaya", date.createDate("16.05.2003"), date.createDate("15.08.2005"));
         TravelCalculatePremiumResponse response = new TravelCalculatePremiumResponse(request);
         TravelCalculatePremiumServiceImpl impl = new TravelCalculatePremiumServiceImpl();
         assert (response.equals(impl.calculatePremium(request)));
