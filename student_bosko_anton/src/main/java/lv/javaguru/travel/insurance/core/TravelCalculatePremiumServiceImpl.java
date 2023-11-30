1468c09f326b586ea7fa21f90a25e807d2e12c08
funkyfall <funkaa15@gmail.com>
2023-11-30 13:33:36 +0300

    STEP 6 = DONE

--- student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -2,13 +2,21 @@ package lv.javaguru.travel.insurance.core;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
+import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.stereotype.Component;
+
+import java.util.List;
 
 @Component
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
+    TravelCalculatePremiumRequestValidator validator;
+
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
+        List<ValidationError> errors = validator.validate(request);
+        if(!errors.isEmpty())
+            return new TravelCalculatePremiumResponse(errors);
         return new TravelCalculatePremiumResponse(request);
     }
 
--- student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_bosko_anton/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -2,6 +2,7 @@ package lv.javaguru.travel.insurance.dto;
 
 import java.math.BigDecimal;
 import java.util.Date;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.TimeUnit;
 
@@ -9,12 +10,13 @@ import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lv.javaguru.travel.insurance.core.DateTimeService;
 
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class TravelCalculatePremiumResponse {
+public class TravelCalculatePremiumResponse extends CoreResponse{
 
     private String personFirstName;
     private String personLastName;
@@ -28,9 +30,11 @@ public class TravelCalculatePremiumResponse {
         this.setPersonLastName(request.getPersonLastName());
         this.setAgreementDateFrom(request.getAgreementDateFrom());
         this.setAgreementDateTo(request.getAgreementDateTo());
-        this.setAgreementPrice(BigDecimal.valueOf(TimeUnit.DAYS.convert(agreementDateTo.getTime() - agreementDateFrom.getTime(),
-                TimeUnit.MILLISECONDS)));
+        DateTimeService daysBetween = new DateTimeService();
+        long difference = daysBetween.getDaysBetween(request.getAgreementDateFrom(), request.getAgreementDateTo());
+        this.setAgreementPrice(new BigDecimal(difference));
     }
+    public TravelCalculatePremiumResponse(List<ValidationError> errors) { super(errors); }
 
     @Override
     public boolean equals(Object obj)
