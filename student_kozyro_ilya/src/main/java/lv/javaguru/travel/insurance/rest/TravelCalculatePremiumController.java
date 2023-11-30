401deee5389992074d32da42d9ffcda5c3a96cd6
animousen4 <ilkozyro@gmail.com>
2023-11-30 22:29:10 +0300

    step_25 = DONE

--- student_kozyro_ilya/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_kozyro_ilya/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -1,7 +1,8 @@
 package lv.javaguru.travel.insurance.rest;
 
 import lv.javaguru.travel.insurance.core.TravelCalculatePremiumService;
-import lv.javaguru.travel.insurance.core.logger.MLogger;
+import lv.javaguru.travel.insurance.core.logger.TravelCalculatePremiumRequestLogger;
+import lv.javaguru.travel.insurance.core.logger.TravelCalculatePremiumResponseLogger;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +10,6 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import static lv.javaguru.travel.insurance.rest.RestConfig.insuranceTravel;
 
 @RestController
 @RequestMapping(RestConfig.insuranceTravel)
@@ -20,13 +19,19 @@ public class TravelCalculatePremiumController {
 	private TravelCalculatePremiumService calculatePremiumService;
 
 	@Autowired
-	MLogger mLogger;
+	TravelCalculatePremiumRequestLogger requestLogger;
+
+	@Autowired
+	TravelCalculatePremiumResponseLogger responseLogger;
+
 	@PostMapping(path = "/",
 			consumes = "application/json",
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
-		mLogger.logTravelPremiumRequest(request);
-		return calculatePremiumService.calculatePremium(request);
+		requestLogger.logRequest(request);
+		var response = calculatePremiumService.calculatePremium(request);
+		responseLogger.logResponse(response);
+		return response;
 	}
 
 }
