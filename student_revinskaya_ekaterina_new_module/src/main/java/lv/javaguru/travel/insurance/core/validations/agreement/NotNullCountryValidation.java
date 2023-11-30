8fa7f517e6469900d9deddee5c968f3b29f9ab24
katierevinska <katerevinska05@gmail.com>
2023-11-30 10:21:31 +0300

    step_95 done

---
+++ student_revinskaya_ekaterina_new_module/src/main/java/lv/javaguru/travel/insurance/core/validations/agreement/NotNullCountryValidation.java
@@ -0,0 +1,27 @@
+package lv.javaguru.travel.insurance.core.validations.agreement;
+
+import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import lv.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
+import lv.javaguru.travel.insurance.core.repositories.ClassifierValueRepository;
+import lv.javaguru.travel.insurance.core.util.Placeholder;
+import lv.javaguru.travel.insurance.core.validations.ValidationErrorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+@Component
+public class NotNullCountryValidation extends TravelAgreementFieldValidationImpl {
+    @Autowired
+    private ValidationErrorFactory validationErrorFactory;
+    @Autowired private ClassifierValueRepository classifierValueRepository;
+    @Override
+    public Optional<ValidationErrorDTO> validate(AgreementDTO request) {//can be validateList later
+        return validateNotNull(request) ? Optional.of(validationErrorFactory.buildError("ERROR_CODE_10",
+                List.of(new Placeholder("SELECTED_RISK", "TRAVEL_MEDICAL"))))
+                : Optional.empty();
+    }
+    private boolean validateNotNull(AgreementDTO request){
+        return (request.getCountry() == null || request.getCountry().isEmpty());
+    }
+}
