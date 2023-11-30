0886dfcf1ccb8c04924aad668135468aaa1f548a
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2023-11-30 11:58:25 +0300

    STEP_65=DONE

--- student_trofimovich_timofey/src/main/java/lv/javaguru/travel/insurance/core/repositories/CountryDefaultDayRateRepository.java
+++ student_trofimovich_timofey/src/main/java/lv/javaguru/travel/insurance/core/repositories/CountryDefaultDayRateRepository.java
@@ -6,10 +6,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface CountryDefaultDayRateRepository extends JpaRepository<CountryDefaultDayRate, Long> {
-    //Optional<CountryDefaultDayRate> findByCountryIc(String countryIc);
-   /* @Query("SELECT cdr from CountryDefaultDayRate cdr " +
-            "where cdr.country_ic = :country_ic ")*/
     Optional<CountryDefaultDayRate> findByCountryIc(String countryIc);
-            //@Param("country_ic") String country_ic
-    //);
 }
