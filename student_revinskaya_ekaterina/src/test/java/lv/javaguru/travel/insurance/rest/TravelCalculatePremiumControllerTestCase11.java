f8c51af9043eda2a3ad00bfca5d711da4a342ffa
katierevinska <katerevinska05@gmail.com>
2023-12-01 18:08:35 +0300

    refactoring_5

--- student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTestCase11.java
+++ student_revinskaya_ekaterina/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTestCase11.java
@@ -3,9 +3,7 @@ package lv.javaguru.travel.insurance.rest;
 import org.junit.jupiter.api.Test;
 
 public class TravelCalculatePremiumControllerTestCase11 extends TravelCalculatePremiumControllerTest{
-    @Test
-    public void testRequestWithNotExistMedicalRiskLimitLevel() throws Exception {
-        equalsJsonFiles("rest/test_case_11/ControllerRequestWithNotExistMedicalRiskLimitLevel.json",
-                "rest/test_case_11/ControllerResponseWithNotExistMedicalRiskLimitLevel.json");
-    }
+    @Override
+    protected String getTestCaseName(){return "rest/test_case_11/ControllerWithNotExistMedicalRiskLimitLevel";}
+
 }
