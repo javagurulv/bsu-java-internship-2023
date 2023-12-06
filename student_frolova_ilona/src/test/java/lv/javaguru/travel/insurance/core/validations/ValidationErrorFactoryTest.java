9d87519e9f7f4fbf9ee7d56b4bbd188f160fdd7c
blackberry020 <ilonafrolova00@gmail.com>
2023-12-06 22:20:28 +0300

    step_41 done

--- student_frolova_ilona/src/test/java/lv/javaguru/travel/insurance/core/validations/ValidationErrorFactoryTest.java
+++ student_frolova_ilona/src/test/java/lv/javaguru/travel/insurance/core/validations/ValidationErrorFactoryTest.java
@@ -1,10 +1,31 @@
 package lv.javaguru.travel.insurance.core.validations;
 
 import lv.javaguru.travel.insurance.core.ErrorManager;
+import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
 import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
 
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
 public class ValidationErrorFactoryTest {
 
     @Mock
     private ErrorManager errorManager;
+
+    @InjectMocks
+    private ValidationErrorFactory errorFactory;
+
+    @Test
+    public void errorCreation() {
+        when(errorManager.getErrorDescription("ERROR_CODE")).thenReturn("description");
+        ValidationError error = errorFactory.buildError("ERROR_CODE");
+
+        assertEquals(error.getErrorCode(), "ERROR_CODE");
+        assertEquals(error.getDescription(), "description");
+    }
 }
