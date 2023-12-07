ee63a4c1f4a02c0fe2467d1859f0e63987398e50
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2023-12-07 10:57:22 +0300

    STEP 119 = DONE

--- student_kalesnik_ivan_v2/src/main/java/lv/javaguru/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
+++ student_kalesnik_ivan_v2/src/main/java/lv/javaguru/travel/insurance/core/services/TravelCalculatePremiumServiceImpl.java
@@ -1,5 +1,6 @@
 package lv.javaguru.travel.insurance.core.services;
 
+import com.google.common.base.Stopwatch;
 import lv.javaguru.travel.insurance.core.api.command.TravelCalculatePremiumCoreCommand;
 import lv.javaguru.travel.insurance.core.api.command.TravelCalculatePremiumCoreResult;
 import lv.javaguru.travel.insurance.core.api.dto.*;
@@ -10,14 +11,18 @@ import lv.javaguru.travel.insurance.core.services.factory.RiskManagerFactorySave
 import lv.javaguru.travel.insurance.core.underwriting.TravelPremiumCalculationResult;
 import lv.javaguru.travel.insurance.core.underwriting.TravelPremiumUnderwriting;
 import lv.javaguru.travel.insurance.core.validations.TravelAgreementValidator;
+import lv.javaguru.travel.insurance.rest.common.TravelCalculatePremiumRequestExecutionTimeLogger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.StopWatch;
 
 import java.math.BigDecimal;
 import java.util.Collection;
 import java.util.List;
 
 @Component
+@Transactional
 class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService {
 
     @Autowired private TravelAgreementValidator agreementValidator;
@@ -32,11 +37,14 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
                 : buildResponse(errors);
     }
 
-    private TravelCalculatePremiumCoreResult buildResponse(List<ValidationErrorDto> errors) {
+    public TravelCalculatePremiumCoreResult buildResponse(List<ValidationErrorDto> errors) {
         return new TravelCalculatePremiumCoreResult(errors);
     }
 
-    private TravelCalculatePremiumCoreResult buildResponse(AgreementDto agreement) {
+
+    public TravelCalculatePremiumCoreResult buildResponse(AgreementDto agreement) {
+        Stopwatch stopWatch = Stopwatch.createStarted();
+        if (!stopWatch.isRunning()) stopWatch.start();
         calculateRiskPremiumsForAllPersons(agreement);
 
         BigDecimal totalAgreementPremium = calculateTotalAgreementPremium(agreement);
@@ -44,12 +52,10 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         agreementEntityFactory.createAgreementEntity(agreement);
         TravelCalculatePremiumCoreResult coreResult = new TravelCalculatePremiumCoreResult();
         coreResult.setAgreement(agreement);
+        TravelCalculatePremiumRequestExecutionTimeLogger logger = new TravelCalculatePremiumRequestExecutionTimeLogger();
+        logger.logExecutionTime(stopWatch);
         return coreResult;
     }
-
-    /* private void saveAllPersons(AgreementDto agreement) {
-        agreement.getPersons().forEach(personDTO -> saver.savePerson(personDTO));
-    }*/
 
     private void calculateRiskPremiumsForAllPersons(AgreementDto agreement) {
         agreement.getPersons().forEach(person -> {
