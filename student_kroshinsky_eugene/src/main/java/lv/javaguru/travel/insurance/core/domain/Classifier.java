b9a7d6f346c2136d2628d20db9f3acab3b865a8e
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2023-12-09 09:51:21 +0300

    step_47 = DONE

---
+++ student_kroshinsky_eugene/src/main/java/lv/javaguru/travel/insurance/core/domain/Classifier.java
@@ -0,0 +1,50 @@
+package lv.javaguru.travel.insurance.core.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class Classifier {
+    @Id
+    @Column(name = "id")
+    @GeneratedValue(strategy= GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false, length = 200)
+    private String title;
+
+    @Column(name = "description", nullable = false, length = 100)
+    private String description;
+
+    public Classifier() {
+    }
+
+    public Classifier(Long id, String title, String description) {
+        this.id = id;
+        this.title = title;
+        this.description = description;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+}
