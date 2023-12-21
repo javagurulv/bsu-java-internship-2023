7d8ab90279bc45c4b103098010e197a421e0c5ae
Ivan Kalesnik <ivan.kalesnik1@gmail.com>
2023-12-21 10:18:38 +0300

    STEP 131-132 = DONE

---
+++ student_kalesnik_ivan_v2/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/cancel/TravelCancelRiskCalculator.java
@@ -0,0 +1,23 @@
+package lv.javaguru.travel.insurance.core.underwriting.calculators.cancel;
+
+import lv.javaguru.travel.insurance.core.api.dto.AgreementDto;
+import lv.javaguru.travel.insurance.core.api.dto.PersonDto;
+import lv.javaguru.travel.insurance.core.underwriting.TravelRiskPremiumCalculator;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+
+@Component
+class TravelCancelRiskCalculator implements TravelRiskPremiumCalculator {
+
+    @Override
+    public BigDecimal calculatePremium(AgreementDto agreement, PersonDto person) {
+        return BigDecimal.ZERO;
+    }
+
+    @Override
+    public String getRiskIc() {
+        return "TRAVEL_CANCELLATION";
+    }
+
+}
