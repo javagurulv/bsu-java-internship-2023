af99f1c605f87cd3cf1527f32e4489dda0b5e99c
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2023-12-29 13:18:37 +0300

    step_98=DONE

--- student_trofimovich_timofey_version_2/src/test/java/lv/javaguru/travel/insurance/core/validations/TravelAgreementValidatorImplTest.java
+++ student_trofimovich_timofey_version_2/src/test/java/lv/javaguru/travel/insurance/core/validations/TravelAgreementValidatorImplTest.java
@@ -1,16 +1,15 @@
 package lv.javaguru.travel.insurance.core.validations;
 
 import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
-import lv.javaguru.travel.insurance.core.api.dto.PersonDTO;
 import lv.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
+import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
-import org.springframework.test.util.ReflectionTestUtils;
 
 import java.util.List;
-import java.util.Optional;
 
 import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
 import static org.mockito.Mockito.mock;
@@ -21,141 +20,65 @@ import static org.mockito.Mockito.when;
 
 public class TravelAgreementValidatorImplTest {
 
+    @Mock
+    private TravelPersonFieldValidator personFieldValidator;
+
+    @Mock
+    private TravelAgreementFieldValidator agreementFieldValidator;
     @InjectMocks
     private TravelAgreementValidatorImpl agreementValidatorImpl;
-    
+
+    private AgreementDTO agreement;
+
+    @BeforeEach
+    void init() {
+        agreement = mock(AgreementDTO.class);
+    }
     
     @Test
     void shouldNotReturnErrors() {
 
-        AgreementDTO agreementDTO = mock(AgreementDTO.class);
-        PersonDTO personDTO = mock(PersonDTO.class);
+        when(personFieldValidator.validate(agreement)).thenReturn(List.of());
+        when(agreementFieldValidator.validate(agreement)).thenReturn(List.of());
 
-        TravelAgreementFieldValidation agreementValidation1 = mock(TravelAgreementFieldValidation.class);
-
-        when(agreementValidation1.validate(agreementDTO)).thenReturn(Optional.empty());
-        when(agreementValidation1.validateList(agreementDTO)).thenReturn(List.of());
-
-        TravelAgreementFieldValidation agreementValidation2 = mock(TravelAgreementFieldValidation.class);
-        when(agreementValidation2.validate(agreementDTO)).thenReturn(Optional.empty());
-        when(agreementValidation2.validateList(agreementDTO)).thenReturn(List.of());
-
-        TravelPersonFieldValidation personVal1 = mock(TravelPersonFieldValidation.class);
-        when(personVal1.validate(personDTO)).thenReturn(Optional.empty());
-        when(personVal1.validateList(personDTO)).thenReturn(List.of());
-
-        TravelPersonFieldValidation personVal2 = mock(TravelPersonFieldValidation.class);
-        when(personVal2.validate(personDTO)).thenReturn(Optional.empty());
-        when(personVal2.validateList(personDTO)).thenReturn(List.of());
-
-        List<PersonDTO> persons = List.of(personDTO);
-        when(agreementDTO.getPersons()).thenReturn(persons);
-
-        List<TravelAgreementFieldValidation> agreementValidationsList = List.of(agreementValidation1, agreementValidation2);
-        List<TravelPersonFieldValidation> personValidationsList = List.of(personVal1, personVal2);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "agreementValidations", agreementValidationsList);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "personValidations", personValidationsList);
-
-        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreementDTO);
+        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreement);
         assertThat(errors.size()).isEqualTo(0);
     }
 
     @Test
-    void shouldReturnAgreementListErrors() {
+    void shouldReturnAgreementFieldErrors() {
 
-        AgreementDTO agreementDTO = mock(AgreementDTO.class);
-        
+        when(agreementFieldValidator.validate(agreement)).thenReturn(List.of(mock(ValidationErrorDTO.class),
+                mock(ValidationErrorDTO.class)));
+        when(personFieldValidator.validate(agreement)).thenReturn(List.of());
 
-        TravelAgreementFieldValidation agreementValidation1 = mock(TravelAgreementFieldValidation.class);
-        when(agreementValidation1.validate(agreementDTO)).thenReturn(Optional.empty());
-        when(agreementValidation1.validateList(agreementDTO)).thenReturn(List.of(mock(ValidationErrorDTO.class)));
-
-        TravelAgreementFieldValidation agreementValidation2 = mock(TravelAgreementFieldValidation.class);
-        when(agreementValidation2.validate(agreementDTO)).thenReturn(Optional.empty());
-        when(agreementValidation2.validateList(agreementDTO)).thenReturn(List.of(mock(ValidationErrorDTO.class)));
-
-        List<TravelAgreementFieldValidation> agreementValidationsList = List.of(agreementValidation1, agreementValidation2);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "agreementValidations", agreementValidationsList);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "personValidations", List.of());
-
-        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreementDTO);
+        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreement);
         assertThat(errors.size()).isGreaterThan(0);
         assertThat(errors.size()).isEqualTo(2);
     }
 
     @Test
-    void shouldReturnListPersonErrors() {
+    void shouldReturnPersonFieldErrors() {
 
-        AgreementDTO agreementDTO = mock(AgreementDTO.class);
-        PersonDTO personDTO = mock(PersonDTO.class);
+        when(personFieldValidator.validate(agreement)).thenReturn(List.of(mock(ValidationErrorDTO.class),
+                mock(ValidationErrorDTO.class)));
+        when(agreementFieldValidator.validate(agreement)).thenReturn(List.of());
 
-        TravelPersonFieldValidation personVal1 = mock(TravelPersonFieldValidation.class);
-        when(personVal1.validate(personDTO)).thenReturn(Optional.empty());
-        when(personVal1.validateList(personDTO)).thenReturn(List.of(mock(ValidationErrorDTO.class)));
-
-        TravelPersonFieldValidation personVal2 = mock(TravelPersonFieldValidation.class);
-        when(personVal2.validate(personDTO)).thenReturn(Optional.empty());
-        when(personVal2.validateList(personDTO)).thenReturn(List.of(mock(ValidationErrorDTO.class)));
-
-        List<PersonDTO> persons = List.of(personDTO);
-        when(agreementDTO.getPersons()).thenReturn(persons);
-
-        List<TravelPersonFieldValidation> personValidationsList = List.of(personVal1, personVal2);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "agreementValidations", List.of());
-        ReflectionTestUtils.setField(agreementValidatorImpl, "personValidations", personValidationsList);
-
-        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreementDTO);
-        assertThat(errors.size()).isEqualTo(2);
-    }
-
-    @Test
-    void shouldReturnSingleAgreementErrors() {
-
-        AgreementDTO agreementDTO = mock(AgreementDTO.class);
-
-        TravelAgreementFieldValidation agreementValidation1 = mock(TravelAgreementFieldValidation.class);
-        when(agreementValidation1.validate(agreementDTO)).thenReturn(Optional.of(mock(ValidationErrorDTO.class)));
-        //when(agreementValidation1.validateList(agreementDTO)).thenReturn(List.of(mock(ValidationError.class)));
-
-        TravelAgreementFieldValidation agreementValidation2 = mock(TravelAgreementFieldValidation.class);
-        when(agreementValidation2.validate(agreementDTO)).thenReturn(Optional.of(mock(ValidationErrorDTO.class)));
-        //when(agreementValidation2.validateList(agreementDTO)).thenReturn(List.of(mock(ValidationError.class)));
-
-
-
-        List<TravelAgreementFieldValidation> agreementValidationsList = List.of(agreementValidation1, agreementValidation2);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "agreementValidations", agreementValidationsList);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "personValidations", List.of());
-
-        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreementDTO);
+        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreement);
         assertThat(errors.size()).isGreaterThan(0);
         assertThat(errors.size()).isEqualTo(2);
     }
 
     @Test
-    void shouldReturnSinglePersonErrors() {
+    void shouldReturnBothAgreementAndPersonErrors() {
 
-        AgreementDTO agreementDTO = mock(AgreementDTO.class);
-        PersonDTO personDTO = mock(PersonDTO.class);
+        when(personFieldValidator.validate(agreement)).thenReturn(List.of(mock(ValidationErrorDTO.class),
+                mock(ValidationErrorDTO.class)));
+        when(agreementFieldValidator.validate(agreement)).thenReturn(List.of(mock(ValidationErrorDTO.class),
+                mock(ValidationErrorDTO.class)));
 
-        TravelPersonFieldValidation personVal1 = mock(TravelPersonFieldValidation.class);
-        when(personVal1.validate(personDTO)).thenReturn(Optional.of(mock(ValidationErrorDTO.class)));
-        /*when(personVal1.validateList(agreementDTO)).thenReturn(List.of(mock(ValidationError.class)));*/
-
-        TravelPersonFieldValidation personVal2 = mock(TravelPersonFieldValidation.class);
-        when(personVal2.validate(personDTO)).thenReturn(Optional.of(mock(ValidationErrorDTO.class)));
-        /*when(personVal2.validateList(agreementDTO)).thenReturn(List.of(mock(ValidationError.class)));*/
-
-        List<PersonDTO> persons = List.of(personDTO);
-        when(agreementDTO.getPersons()).thenReturn(persons);
-
-        List<TravelPersonFieldValidation> personValidationsList = List.of(personVal1, personVal2);
-        ReflectionTestUtils.setField(agreementValidatorImpl, "agreementValidations", List.of());
-        ReflectionTestUtils.setField(agreementValidatorImpl, "personValidations", personValidationsList);
-
-        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreementDTO);
+        List<ValidationErrorDTO> errors = agreementValidatorImpl.validate(agreement);
         assertThat(errors.size()).isGreaterThan(0);
-        assertThat(errors.size()).isEqualTo(2);
+        assertThat(errors.size()).isEqualTo(4);
     }
-
 }
