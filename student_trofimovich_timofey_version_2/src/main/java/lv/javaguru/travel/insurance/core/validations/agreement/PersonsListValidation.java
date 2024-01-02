f485dde9e7a21669f277bec9606b5300cb640888
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2024-01-02 21:16:04 +0300

    step_106=DONE

---
+++ student_trofimovich_timofey_version_2/src/main/java/lv/javaguru/travel/insurance/core/validations/agreement/PersonsListValidation.java
@@ -0,0 +1,22 @@
+package lv.javaguru.travel.insurance.core.validations.agreement;
+
+import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import lv.javaguru.travel.insurance.core.api.dto.ValidationErrorDTO;
+import lv.javaguru.travel.insurance.core.validations.ValidationErrorFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.util.Optional;
+
+@Component
+class PersonsListValidation extends TravelAgreementFieldValidationImpl {
+    @Autowired
+    private ValidationErrorFactory factory;
+
+    @Override
+    public Optional<ValidationErrorDTO> validate(AgreementDTO agreement) {
+        return (agreement.getPersons() == null || agreement.getPersons().isEmpty())
+                ? Optional.of(factory.buildError("ERROR_CODE_16"))
+                : Optional.empty();
+    }
+}
