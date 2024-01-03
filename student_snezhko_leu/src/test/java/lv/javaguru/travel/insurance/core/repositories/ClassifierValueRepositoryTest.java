ff9d9dee13a75351011b030c742f97baea760050
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2024-01-03 13:31:49 +0200

    step_49=DONE

--- student_snezhko_leu/src/test/java/lv/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
+++ student_snezhko_leu/src/test/java/lv/javaguru/travel/insurance/core/repositories/ClassifierValueRepositoryTest.java
@@ -24,58 +24,40 @@ public class ClassifierValueRepositoryTest {
     public void shouldFind_RiskType_TRAVEL_MEDICAL(){
         String ic = "TRAVEL_MEDICAL";
         String classifier = "RISK_TYPE";
-        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
-        assertTrue(cv.isPresent());
-        assertEquals(cv.get().getIc(), ic);
-        assertEquals(cv.get().getClassifier().getTitle(), classifier);
+        test(classifier, ic);
     }
 
     public void shouldFind_RiskType_TRAVEL_CANCELLATION() {
         String ic = "TRAVEL_CANCELLATION";
         String classifier = "RISK_TYPE";
-        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
-        assertTrue(cv.isPresent());
-        assertEquals(cv.get().getIc(), ic);
-        assertEquals(cv.get().getClassifier().getTitle(), classifier);
+        test(classifier, ic);
     }
 
     public void shouldFind_RiskType_TRAVEL_LOSS_BAGGAGE() {
         String ic = "TRAVEL_LOSS_BAGGAGE";
         String classifier = "RISK_TYPE";
-        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
-        assertTrue(cv.isPresent());
-        assertEquals(cv.get().getIc(), ic);
-        assertEquals(cv.get().getClassifier().getTitle(), classifier);
+        test(classifier, ic);
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_THIRD_PARTY_LIABILITY() {
         String ic = "TRAVEL_THIRD_PARTY_LIABILITY";
         String classifier = "RISK_TYPE";
-        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
-        assertTrue(cv.isPresent());
-        assertEquals(cv.get().getIc(), ic);
-        assertEquals(cv.get().getClassifier().getTitle(), classifier);
+        test(classifier, ic);
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_EVACUATION() {
         String ic = "TRAVEL_EVACUATION";
         String classifier = "RISK_TYPE";
-        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
-        assertTrue(cv.isPresent());
-        assertEquals(cv.get().getIc(), ic);
-        assertEquals(cv.get().getClassifier().getTitle(), classifier);
+        test(classifier, ic);
     }
 
     @Test
     public void shouldFind_RiskType_TRAVEL_SPORT_ACTIVITIES() {
         String ic = "TRAVEL_SPORT_ACTIVITIES";
         String classifier = "RISK_TYPE";
-        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
-        assertTrue(cv.isPresent());
-        assertEquals(cv.get().getIc(), ic);
-        assertEquals(cv.get().getClassifier().getTitle(), classifier);
+        test(classifier, ic);
     }
 
     @Test
@@ -84,5 +66,11 @@ public class ClassifierValueRepositoryTest {
         String classifier = "RISK_TYPE";
         Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
         assertTrue(cv.isEmpty());
+    }
+    public void test(String classifier, String ic) {
+        Optional<ClassifierValue> cv = classifierValueRepository.findByClassifierTitleAndIc(classifier, ic);
+        assertTrue(cv.isPresent());
+        assertEquals(cv.get().getIc(), ic);
+        assertEquals(cv.get().getClassifier().getTitle(), classifier);
     }
 }
