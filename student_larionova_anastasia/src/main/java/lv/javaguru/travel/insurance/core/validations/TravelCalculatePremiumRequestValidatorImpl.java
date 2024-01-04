b7b378db4cdb9cc938455fb3b78b4066865c8e5a
LarionovaAnastasiaAlexandrovna <larionova.anastasia.alexandrovna@gmail.com>
2024-01-04 16:58:50 +0300

    STEP 22 = DONE

--- student_larionova_anastasia/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImpl.java
+++ student_larionova_anastasia/src/main/java/lv/javaguru/travel/insurance/core/validations/TravelCalculatePremiumRequestValidatorImpl.java
@@ -1,20 +1,21 @@
-package lv.javaguru.travel.insurance.core;
+package lv.javaguru.travel.insurance.core.validations;
 
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.ValidationError;
-import lv.javaguru.travel.insurance.core.validations.*;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
-
+import java.util.stream.Collectors;
 
 @Component
-public class TravelCalculatePremiumRequestValidator {
+/*public */class TravelCalculatePremiumRequestValidatorImpl
+        implements TravelCalculatePremiumRequestValidator {
 
     @Autowired private List<TravelRequestValidator> travelValidations;
+    @Override
     public List<ValidationError> validate(TravelCalculatePremiumRequest request) {
         List<ValidationError> errors = new ArrayList<>();
         for (var validation : travelValidations) {
