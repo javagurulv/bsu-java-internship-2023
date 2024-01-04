310d9af553b102fb25bd0b8f13cf04ba340d8f15
LarionovaAnastasiaAlexandrovna <larionova.anastasia.alexandrovna@gmail.com>
2024-01-05 00:01:17 +0300

    STEP 25 = DONE

--- student_larionova_anastasia/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
+++ student_larionova_anastasia/src/main/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumController.java
@@ -4,16 +4,14 @@ import lv.javaguru.travel.insurance.core.TravelCalculatePremiumService;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/insurance/travel")
 public class TravelCalculatePremiumController {
 
 	@Autowired private TravelCalculatePremiumRequestLoggerImpl requestLogger;
+	@Autowired private TravelCalculatePremiumResponseLoggerImpl responseLogger;
 	@Autowired private TravelCalculatePremiumService calculatePremiumService;
 
 	@PostMapping(path = "/",
@@ -21,6 +19,8 @@ public class TravelCalculatePremiumController {
 			produces = "application/json")
 	public TravelCalculatePremiumResponse calculatePremium(@RequestBody TravelCalculatePremiumRequest request) {
 		requestLogger.logRequest(request);
+		TravelCalculatePremiumResponse response = calculatePremiumService.calculatePremium(request);
+		responseLogger.logResponse(response);
 		return calculatePremiumService.calculatePremium(request);
 	}
 
