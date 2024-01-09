b06ea40beebc594c209eb6a623e5fbb26094b98b
dcher <cernookijdima7@gmail.com>
2024-01-09 20:51:50 +0300

    Step_12=DONE

--- student_chernookiy_dmitry/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
+++ student_chernookiy_dmitry/src/main/java/lv/javaguru/travel/insurance/core/TravelCalculatePremiumServiceImpl.java
@@ -3,6 +3,7 @@ package lv.javaguru.travel.insurance.core;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumResponse;
 import lv.javaguru.travel.insurance.dto.ValidationError;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
@@ -18,10 +19,18 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
 
     TravelCalculatePremiumRequestValidator validator = new TravelCalculatePremiumRequestValidator();
 
+    @Autowired
+    public TravelCalculatePremiumServiceImpl(TravelCalculatePremiumRequestValidator requestValidator) {
+        this.validator = requestValidator;
+    }
 
     @Override
     public TravelCalculatePremiumResponse calculatePremium(TravelCalculatePremiumRequest request) {
         List<ValidationError> errors = validator.validate(request);
+        return buildResponse(request, errors);
+
+    }
+    public TravelCalculatePremiumResponse buildResponse(TravelCalculatePremiumRequest request, List<ValidationError> errors) {
         if (!errors.isEmpty()) {
             return new TravelCalculatePremiumResponse(errors);
         }
@@ -40,11 +49,10 @@ class TravelCalculatePremiumServiceImpl implements TravelCalculatePremiumService
         return date.toInstant().atZone(ZoneId.systemDefault()).toLocalDate();
     }
 
-    private static BigDecimal calculateAgreementPrice(Date from, Date to) {
+    public static BigDecimal calculateAgreementPrice(Date from, Date to) {
         LocalDate localDateFrom = dateToLocalDate(from);
         LocalDate localDateTo = dateToLocalDate(to);
         Period period = Period.between(localDateFrom, localDateTo);
         return new BigDecimal(period.getDays());
     }
-
 }
