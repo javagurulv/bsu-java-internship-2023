3fc779fe58d33ac28e94ddec4f569a13a62b2001
Timofey Trofimovich <trofimovichtimofey90210@gmail.com>
2024-01-13 16:04:37 +0300

    step_117=DONE

--- student_trofimovich_timofey_version_2/src/main/java/lv/javaguru/travel/insurance/core/repositories/SelectedRiskEntityRepository.java
+++ student_trofimovich_timofey_version_2/src/main/java/lv/javaguru/travel/insurance/core/repositories/SelectedRiskEntityRepository.java
@@ -2,9 +2,8 @@ package lv.javaguru.travel.insurance.core.repositories;
 
 import lv.javaguru.travel.insurance.core.domain.entities.SelectedRiskEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Component;
 
-@Component
+
 public interface SelectedRiskEntityRepository extends JpaRepository<SelectedRiskEntity, Long> {
 
 }
