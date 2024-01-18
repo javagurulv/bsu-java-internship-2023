9d5fd8e0e133e530ad2cf248f9446be994a69948
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2024-01-18 13:17:38 +0200

    step_65=DONE

--- student_snezhko_leu/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
+++ student_snezhko_leu/src/main/java/lv/javaguru/travel/insurance/dto/TravelCalculatePremiumResponse.java
@@ -5,8 +5,10 @@ import java.util.Date;
 import java.util.List;
 
 import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
 import lombok.*;
 import lv.javaguru.travel.insurance.core.ValidationError;
+import lv.javaguru.travel.insurance.core.util.BigDecimalSerializer;
 import lv.javaguru.travel.insurance.rest.CoreResponse;
 
 @Getter
@@ -21,7 +23,10 @@ public class TravelCalculatePremiumResponse extends CoreResponse {
     private Date agreementDateFrom;
     @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd")
     private Date agreementDateTo;
+
+    @JsonSerialize(using = BigDecimalSerializer.class)
     private BigDecimal agreementPremium;
+
     private List<TravelCalculatePremiumRisk> risks;
     private String country;
 //    private List<InsurancePremiumRisk> risks;
