d32fae7dbf56d9e6cea115c58402278ad9282c93
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2024-01-23 20:54:18 +0200

    step_78=DONE

--- student_snezhko_leu/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/medical/TravelCalculateInsuranceLimitCoefficient.java
+++ student_snezhko_leu/src/main/java/lv/javaguru/travel/insurance/core/underwriting/calculators/medical/TravelCalculateInsuranceLimitCoefficient.java
@@ -5,17 +5,29 @@ import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
+import java.io.IOException;
+
+import static lv.javaguru.travel.insurance.core.util.CheckApplicationPropertiesUtil.checkProperty;
+
 @Component
 class TravelCalculateInsuranceLimitCoefficient {
     @Autowired
     private MedicalRiskLimitLevelRepository mrllRepository;
 
     public Double calculatePremium(TravelCalculatePremiumRequest request) {
-        return mrllRepository
-                .findCoefficientByLimitLevelIc(
-                        request.getMedicalRiskLimitLevel()
-                )
-                .get()
-                .getCoefficient();
+        try {
+            return checkProperty("medical.risk.limit.level.enabled")
+                    ? mrllRepository
+                        .findCoefficientByLimitLevelIc
+                                (
+                                    request.getMedicalRiskLimitLevel()
+                                )
+                        .get()
+                        .getCoefficient()
+                    : 1d;
+        }
+        catch (IOException e) {
+            return 1d;
+        }
     }
 }
