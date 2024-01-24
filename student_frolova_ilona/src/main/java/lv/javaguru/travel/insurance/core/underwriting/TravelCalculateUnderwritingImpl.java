631ae367c90c17709c7e842e44cb2ad038d5f51d
blackberry020 <ilonafrolova00@gmail.com>
2024-01-24 13:20:59 +0300

    step_55 done + riskPremiumCalculators

--- student_frolova_ilona/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelCalculateUnderwritingImpl.java
+++ student_frolova_ilona/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelCalculateUnderwritingImpl.java
@@ -1,20 +1,27 @@
 package lv.javaguru.travel.insurance.core.underwriting;
 
+import lombok.RequiredArgsConstructor;
 import lv.javaguru.travel.insurance.core.util.DateTimeUtil;
 import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.convert.ReadingConverter;
 import org.springframework.stereotype.Component;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Component
+@RequiredArgsConstructor
 class TravelCalculateUnderwritingImpl implements TravelCalculateUnderwriting {
 
-    @Autowired
-    private DateTimeUtil dateTimeUtil;
+    private final List<TravelRiskPremiumCalculator> riskPremiumCalculators;
 
     @Override
     public BigDecimal calculatePremium(TravelCalculatePremiumRequest request) {
-        return dateTimeUtil.getDifferenceInDays(request.getAgreementDateFrom(), request.getAgreementDateTo());
+
+        return riskPremiumCalculators.stream()
+                .filter(x -> request.getSelectedRisks().contains(x.getRiskIc()))
+                .map(x -> x.calculatePremium(request))
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
     }
 }
