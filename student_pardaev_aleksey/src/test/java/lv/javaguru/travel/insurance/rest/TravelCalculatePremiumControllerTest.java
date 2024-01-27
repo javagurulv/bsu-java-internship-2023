4b8d5dee134598d39dbebe9e16868cfb7f49d321
Aleksey Pardaev <lpardaev607@gmail.com>
2024-01-28 02:30:01 +0300

    step_18-19=DONE

--- student_pardaev_aleksey/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_pardaev_aleksey/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -1,5 +1,6 @@
 package lv.javaguru.travel.insurance.rest;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,10 +10,10 @@ import org.springframework.http.HttpHeaders;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.MvcResult;
 
-import static org.hamcrest.Matchers.is;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @ExtendWith(SpringExtension.class)
@@ -21,24 +22,46 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 public class TravelCalculatePremiumControllerTest {
 
     @Autowired private MockMvc mockMvc;
+    @Autowired private JsonReader jsonReader;
+    private ObjectMapper mapper = new ObjectMapper();
 
-    @Test
-    public void simpleRestControllerTest() throws Exception {
-        mockMvc.perform(post("/insurance/travel/")
-                        .content("{" +
-                                "\"personFirstName\" : \"Vasja\",\n" +
-                                "\"personLastName\" : \"Pupkin\",\n" +
-                                "\"agreementDateFrom\" : \"2021-05-25\",\n" +
-                                "\"agreementDateTo\" : \"2021-05-29\"\n" +
-                                "}")
+    public void compare(String requestPath, String responsePath) throws Exception {
+        MvcResult result = mockMvc.perform(post("/insurance/travel/")
+                        .content(jsonReader.readJsonToString(requestPath))
                         .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE))
                 .andExpect(status().isOk())
-                .andExpect(jsonPath("personFirstName", is("Vasja")))
-                .andExpect(jsonPath("personLastName", is("Pupkin")))
-                .andExpect(jsonPath("agreementDateFrom", is("2021-05-25")))
-                .andExpect(jsonPath("agreementDateTo", is("2021-05-29")))
-                .andExpect(jsonPath("agreementPrice", is(4)))
                 .andReturn();
+        assertEquals(mapper.readTree(result.getResponse().getContentAsString()),
+                mapper.readTree(jsonReader.readJsonToString(responsePath)));
     }
 
+    @Test
+    public void everythingProvided() throws Exception {
+        compare("rest/TravelCalculatePremiumRequest.json", "rest/TravelCalculatePremiumResponse.json");
+    }
+
+    @Test
+    public void firstNameNotProvided() throws Exception{
+        compare("rest/FirstNameNotProvidedRequest.json", "rest/FirstNameNotProvidedResponse.json");
+    }
+
+    @Test
+    public void lastNameNotProvided() throws Exception{
+        compare("rest/LastNameNotProvidedRequest.json", "rest/LastNameNotProvidedResponse.json");
+    }
+
+    @Test
+    public void dateToNotProvided() throws Exception{
+        compare("rest/DateToNotProvidedRequest.json", "rest/DateToNotProvidedResponse.json");
+    }
+
+    @Test
+    public void DateFromNotProvided() throws Exception{
+        compare("rest/DateFromNotProvidedRequest.json", "rest/DateFromNotProvidedResponse.json");
+    }
+
+    @Test
+    public void dateToLessThanDateFrom() throws Exception{
+        compare("rest/DateToLessThanDateFromRequest.json", "rest/DateToLessThanDateFromResponse.json");
+    }
 }
