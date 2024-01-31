75f13b29dce6b2cac0d63996ecdf252498f58c65
Ivan Filon <vanbest27@gmail.com>
2024-01-31 17:43:25 +0300

    step_20 = DONE

--- student_filon_ivan/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
+++ student_filon_ivan/src/test/java/lv/javaguru/travel/insurance/rest/TravelCalculatePremiumControllerTest.java
@@ -71,6 +71,17 @@ public class TravelCalculatePremiumControllerTest {
                 "rest/ResponseDateToLessFrom.json");
     }
 
+    @Test
+    public void dateFromLessCurrentDate() throws Exception {
+        performTest("rest/RequestDateFromLessCurrentDate.json",
+                "rest/ResponseDateFromLessCurrentDate.json");
+    }
+
+    @Test
+    public void dateFromLessCurrentDateAndDateToLessDateFrom() throws Exception {
+        performTest("rest/RequestDateFromLessCurrentDateAndDateToLessDateFrom.json",
+                "rest/ResponseDateFromLessCurrentDateAndDateToLessDateFrom.json");
+    }
     public void performTest(String requestPath, String responsePath) throws Exception {
         String request = reader.readJson(requestPath);
 
