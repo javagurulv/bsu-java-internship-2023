431e68c6fdf062a0184645881a5f8e60ff38d169
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2024-02-02 23:09:28 +0300

    step_64 = DONE

---
+++ student_kroshinsky_eugene/src/test/java/lv/javaguru/travel/insurance/core/repositories/CountryDefaultDayRateRepositoryTest.java
@@ -0,0 +1,45 @@
+package lv.javaguru.travel.insurance.core.repositories;
+
+import lv.javaguru.travel.insurance.core.domain.ClassifierValue;
+import lv.javaguru.travel.insurance.core.domain.CountryDefaultDayRate;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Optional;
+
+import static org.junit.jupiter.api.Assertions.*;
+@ExtendWith(SpringExtension.class)
+@DataJpaTest
+class CountryDefaultDayRateRepositoryTest {
+    @Autowired
+    private CountryDefaultDayRateRepository countryDefaultDayRateRepository;
+    @Test
+    public void injectedRepositoryAreNotNull() {
+        assertNotNull(countryDefaultDayRateRepository);
+    }
+    @Test
+    void findLatvia() {
+        shouldFindByIc("LATVIA");
+    }
+    @Test
+    void findSpain() {
+        shouldFindByIc("SPAIN");
+    }
+    @Test
+    void findJapan() {
+        shouldFindByIc("JAPAN");
+    }
+    @Test
+    public void shouldNotFind_RiskType_FAKE() {
+        Optional<CountryDefaultDayRate> valueOpt = countryDefaultDayRateRepository.findByIc("FAKE");
+        assertTrue(valueOpt.isEmpty());
+    }
+    private void shouldFindByIc(String ic) {
+        Optional<CountryDefaultDayRate> valueOptional = countryDefaultDayRateRepository.findByIc(ic);
+        assertTrue(valueOptional.isPresent());
+        assertEquals(ic, valueOptional.get().getIc());
+    }
+}
