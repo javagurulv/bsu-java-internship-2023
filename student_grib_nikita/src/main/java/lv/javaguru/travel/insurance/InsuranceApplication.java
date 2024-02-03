d9b9a6c9e2332e99ab22b727eff973748d94a37e
ektogod <ektogod@mail.ru>
2024-02-03 03:19:40 +0300

    step_20 = done

--- student_grib_nikita/src/main/java/lv/javaguru/travel/insurance/InsuranceApplication.java
+++ student_grib_nikita/src/main/java/lv/javaguru/travel/insurance/InsuranceApplication.java
@@ -1,7 +1,12 @@
 package lv.javaguru.travel.insurance;
 
+import lv.javaguru.travel.insurance.dto.TravelCalculatePremiumRequest;
+import lv.javaguru.travel.insurance.dto.ValidationError;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Date;
+import java.util.List;
 
 @SpringBootApplication
 public class InsuranceApplication {
