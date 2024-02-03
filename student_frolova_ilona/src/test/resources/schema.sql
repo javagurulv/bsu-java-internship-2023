2919602e6d6168b98db75cd3fb9f9deaa1af188b
blackberry020 <ilonafrolova00@gmail.com>
2024-02-03 14:32:45 +0300

    step_69 done

--- student_frolova_ilona/src/test/resources/schema.sql
+++ student_frolova_ilona/src/test/resources/schema.sql
@@ -35,3 +35,9 @@ CREATE UNIQUE INDEX ix_country_default_day_rate_id
 CREATE UNIQUE INDEX ix_country_default_day_rate_country_ic
     ON country_default_day_rate(country_ic);
 
+CREATE TABLE age_coefficient (
+    age_from INT NOT NULL,
+    age_to INT NOT NULL,
+    coefficient DECIMAL(5,2) NOT NULL
+ );
+
