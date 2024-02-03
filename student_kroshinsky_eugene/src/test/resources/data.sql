05362d35bd5a85f24922af3a493e0a4291770c1e
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2024-02-03 22:29:28 +0300

    step_73 = DONE

--- student_kroshinsky_eugene/src/test/resources/data.sql
+++ student_kroshinsky_eugene/src/test/resources/data.sql
@@ -123,3 +123,12 @@ VALUES  (0, 5, 1.1),
         (18, 40, 1.1),
         (41, 65, 1.2),
         (66, 150, 1.5);
+
+INSERT INTO classifiers (title, description)
+VALUES ('MEDICAL_RISK_LIMIT_LEVEL', 'maximum payout to client in insured event');
+
+INSERT INTO classifier_values (classifier_id, ic, description)
+VALUES ((SELECT id FROM `classifiers` WHERE title = 'MEDICAL_RISK_LIMIT_LEVEL'), 'LEVEL_10000', 'Limit is 10000'),
+ ((SELECT id FROM `classifiers` WHERE title = 'MEDICAL_RISK_LIMIT_LEVEL'), 'LEVEL_15000', 'Limit is 15000'),
+ ((SELECT id FROM `classifiers` WHERE title = 'MEDICAL_RISK_LIMIT_LEVEL'), 'LEVEL_20000', 'Limit is 20000'),
+ ((SELECT id FROM `classifiers` WHERE title = 'MEDICAL_RISK_LIMIT_LEVEL'), 'LEVEL_50000', 'Limit is 50000');
