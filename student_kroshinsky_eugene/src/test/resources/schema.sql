24fde9256d2f6534342e66845d4a5cdb0c3551d6
Eugene Kroshinsky <EugeneKroshinsky@gmail.com>
2024-02-06 18:30:40 +0300

    step_76 = DONE

--- student_kroshinsky_eugene/src/test/resources/schema.sql
+++ student_kroshinsky_eugene/src/test/resources/schema.sql
@@ -46,3 +46,13 @@ ON age_coefficient (age_from);
 
 CREATE UNIQUE INDEX ix_age_coefficient_age_to
 ON age_coefficient (age_to);
+
+CREATE TABLE IF NOT EXISTS medical_risk_limit_level (
+  id BIGINT NOT NULL AUTO_INCREMENT,
+  medical_risk_limit_level_ic VARCHAR(200) NOT NULL,
+  coefficient DECIMAL(10,2) NOT NULL,
+  PRIMARY KEY (id)
+);
+
+CREATE UNIQUE INDEX ix_medical_risk_limit_level_ic
+ON medical_risk_limit_level (medical_risk_limit_level_ic);
