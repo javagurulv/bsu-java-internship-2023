d3b50c1b6029e44708aed2d908e7af1e62d796fa
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2024-02-18 20:23:12 +0200

    step_101=DONE

---
+++ student_snezhko_leu_version_2/src/main/java/lv/javaguru/travel/insurance/core/underwriting/TravelUnderwritingImpl.java
@@ -0,0 +1,37 @@
+package lv.javaguru.travel.insurance.core.underwriting;
+
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import lv.javaguru.travel.insurance.core.api.dto.AgreementDTO;
+import lv.javaguru.travel.insurance.core.api.dto.PersonDTO;
+import lv.javaguru.travel.insurance.core.api.dto.RiskDTO;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@Component
+//@AllArgsConstructor
+class TravelUnderwritingImpl implements TravelUnderwriting{
+    //@Autowired
+    //private List<TravelRiskPremiumCalculator> riskCalculators;
+
+    @Autowired
+    private SelectedRisksPremiumCalculator calculator;
+    public BigDecimal calculatePremium(AgreementDTO agreement, PersonDTO person) {
+
+        BigDecimal result = BigDecimal.ZERO;
+        List<RiskDTO> risks = calculator.calculatePremiumForAllRisks(agreement, person);
+
+        for (RiskDTO risk : risks) {
+            result = result.add(risk.getPremium());
+        }
+
+        return result;
+    }
+}
