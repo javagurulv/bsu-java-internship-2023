ff09bbdedc9fa7b3bd7d1a537c61f087214f1df4
Lev Snezhko <fpm.snezhkoLV@bsu.by>
2024-06-14 12:52:01 +0300

    step_148=DONE

---
+++ student_snezhko_leu_version_2/src/test/java/lv/javaguru/travel/insurance/loadtesting/RestCallExample.java
@@ -0,0 +1,64 @@
+package lv.javaguru.travel.insurance.loadtesting;
+
+import com.google.common.base.Stopwatch;
+import lv.javaguru.travel.insurance.rest.JsonFileReader;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.skyscreamer.jsonassert.Customization;
+import org.skyscreamer.jsonassert.JSONAssert;
+import org.skyscreamer.jsonassert.JSONCompareMode;
+import org.springframework.http.*;
+import org.springframework.web.client.RestTemplate;
+
+import java.io.IOException;
+
+import static lv.javaguru.travel.insurance.rest.RemoveRandomValues.removeRandomValues;
+
+
+public class RestCallExample {
+
+    public static void main(String[] args) {
+        try {
+//        executeCall("http://localhost:8080/insurance/travel/api/v2", "rest/v2/correct_request/correct request TM/request.json","rest/v2/correct_request/correct request TM/response.json");
+
+            JsonFileReader jsonReader = new JsonFileReader();
+
+            String urlV1 = "http://localhost:8080/insurance/travel/api/v1/";
+            String urlV2 = "http://localhost:8080/insurance/travel/api/v2/";
+
+            String requestV1Path = "rest/v1/correct_request/all fields filled/request.json";
+            String responseV1Path = "rest/v1/correct_request/all fields filled/response.json";
+
+            String requestV2Path = "rest/v2/correct_request/correct request TM/request.json";
+            String responseV2Path = "rest/v2/correct_request/correct request TM/response.json";
+
+            String requestV1Body = jsonReader.readJsonFromFile(requestV1Path);
+            String expectedResponseV1Body = jsonReader.readJsonFromFile(responseV1Path);
+
+            String requestV2Body = jsonReader.readJsonFromFile(requestV2Path);
+            String expectedResponseV2Body = jsonReader.readJsonFromFile(responseV2Path);
+
+            HttpHeaders headers = new HttpHeaders();
+            headers.setContentType(MediaType.APPLICATION_JSON);
+
+            HttpEntity<String> httpEntityV1 = new HttpEntity<>(requestV1Body, headers);
+            HttpEntity<String> httpEntityV2 = new HttpEntity<>(requestV2Body, headers);
+
+            RestTemplate restTemplate = new RestTemplate();
+
+            String responseV1Body = removeRandomValues(restTemplate.postForObject(urlV1, httpEntityV1, String.class));
+            String responseV2Body = removeRandomValues(restTemplate.postForObject(urlV2, httpEntityV2, String.class));
+
+
+            JSONAssert.assertEquals(expectedResponseV1Body, responseV1Body, JSONCompareMode.LENIENT);
+            JSONAssert.assertEquals(expectedResponseV2Body, responseV2Body, JSONCompareMode.LENIENT);
+            //JSONAssert.assertEquals(expectedResponseBody, responseBody, new CustomComparator(JSONCompareMode.LENIENT, new Customization("uuid", (u1, u2) -> true)));
+
+             }
+
+
+        catch(JSONException | IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
